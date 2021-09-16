a94aaa717e27e35b44a855e429ecb492b88c7c0f
geguodong <37557614+geguodong@users.noreply.github.com>
2021-09-16 04:15:47 +0000

    Sync LeetCode submission - Climbing Stairs (java)

---
+++ my-folder/problems/climbing_stairs/solution.java
@@ -0,0 +1,20 @@
+/*
+Time complexity : O(n). Single loop upto nn is required to calculate n^th
+  fibonacci number.
+Space complexity : O(1). Constant space is used.
+*/
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
