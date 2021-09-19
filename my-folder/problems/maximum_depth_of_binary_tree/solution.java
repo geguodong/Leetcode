fbd53e4a823aa2432001d1eed188961c4008f208
geguodong <37557614+geguodong@users.noreply.github.com>
2021-09-19 22:47:13 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (java)

---
+++ my-folder/problems/maximum_depth_of_binary_tree/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*
+Time complexity : we visit each node exactly once, thus the time complexity is O(N), where NN is the number of nodes.
+
+Space complexity : in the worst case, the tree is completely unbalanced, e.g. each node has only left child node, the recursion call would occur NN times (the height of the tree), therefore the storage to keep the call stack would be O(N). But in the best case (the tree is completely balanced), the height of the tree would be O(log(N)). Therefore, the space complexity in this case would be O(log(N)).
+*/
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+    
+    
+}
