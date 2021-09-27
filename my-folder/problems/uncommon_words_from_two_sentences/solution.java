e61db94d8c29bf45516fe7f3dad6865f718a33d8
geguodong <37557614+geguodong@users.noreply.github.com>
2021-09-27 20:48:25 +0000

    Sync LeetCode submission - Uncommon Words from Two Sentences (java)

---
+++ my-folder/problems/uncommon_words_from_two_sentences/solution.java
@@ -0,0 +1,24 @@
+/*
+Time Complexity: O(M + N)O, where M, NM,N are the lengths of A and B respectively.
+Space Complexity: O(M + N)
+*/
+class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) {
+        Map<String, Integer> count = new HashMap<>();
+        for (String word : s1.split(" ")) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        
+        for (String word : s2.split(" ")) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        
+        List<String> res = new ArrayList<>();
+        for (String word : count.keySet()) {
+            if (count.get(word) == 1) {
+                res.add(word);
+            }
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
