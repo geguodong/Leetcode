a3959b5ea22db09cbb6d8cc82e727b17970bb276
geguodong <37557614+geguodong@users.noreply.github.com>
2021-10-18 05:03:50 +0000

    Sync LeetCode submission - Add Strings (java)

---
+++ my-folder/problems/add_strings/solution.java
@@ -0,0 +1,28 @@
+/*
+Time Complexity: max(N_1, N_2)), where N_1 and N_2 are length of nums1 and nums2. 
+Space Complexity: max(N_1, N_2), because the length of the new string is at most max(N_1, N_2)+1.
+*/
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+
+        int carry = 0;
+        int p1 = num1.length() - 1;
+        int p2 = num2.length() - 1;
+        while (p1 >= 0 || p2 >= 0) {
+            int x1 = p1 >= 0 ? num1.charAt(p1) - '0' : 0;
+            int x2 = p2 >= 0 ? num2.charAt(p2) - '0' : 0;
+            int value = (x1 + x2 + carry) % 10;
+            carry = (x1 + x2 + carry) / 10;
+            res.append(value);
+            p1--;
+            p2--;    
+        }
+        
+        if (carry != 0)
+            res.append(carry);
+        
+        return res.reverse().toString();
+    }
+}
