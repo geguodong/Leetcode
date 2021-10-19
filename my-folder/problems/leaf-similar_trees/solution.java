19c7f9ed817050cc0f0471fba810c94fca07ce7e
geguodong <37557614+geguodong@users.noreply.github.com>
2021-10-19 03:22:08 +0000

    Sync LeetCode submission - Leaf-Similar Trees (java)

---
+++ my-folder/problems/leaf-similar_trees/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*
+DFS
+Time Complexity: O(T_1 + T_2) where T_1, T_2T are the lengths of the given trees.
+Space Complexity: O(T_1 + T_2) the space used in storing the leaf values.
+*/
+
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList();
+        List<Integer> leaves2 = new ArrayList();
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+        return leaves1.equals(leaves2);
+    }
+
+    public void dfs(TreeNode node, List<Integer> leafValues) {
+        if (node != null) {
+            if (node.left == null && node.right == null)
+                leafValues.add(node.val);
+            dfs(node.left, leafValues);
+            dfs(node.right, leafValues);
+        }
+    }
+}
