d6ca86068a0f01451d2862a2ff1592c06d0f60bd
geguodong <37557614+geguodong@users.noreply.github.com>
2021-10-24 06:04:09 +0000

    Sync LeetCode submission - Find Mode in Binary Search Tree (java)

---
+++ my-folder/problems/find_mode_in_binary_search_tree/solution.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public void inOrder(TreeNode tree) {
+        if (tree != null) {
+            inOrder(tree.left);
+            list.add(tree.val);
+            inOrder(tree.right);
+        }
+    }
+    public int[] findMode(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inOrder(root);
+        
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int val : list) {
+            map.put(val, map.getOrDefault(val, 0) + 1);
+            if (map.get(val) > max) {
+                max = map.get(val);
+            }
+        }
+        
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            if (entry.getValue() == max) {
+                res.add(entry.getKey());
+            }
+        }
+        int[] result = new int[res.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+}
