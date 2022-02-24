c11e7c3d6612360862b21338f86acc3f1a1547b5
geguodong <37557614+geguodong@users.noreply.github.com>
2022-02-24 03:56:01 +0000

    Sync LeetCode submission - Happy Number (java)

---
+++ my-folder/problems/happy_number/solution.java
@@ -0,0 +1,31 @@
+/*
+Hardcoding the Only Cycle (Advanced)
+
+O(logn)
+O(1)
+
+*/
+
+class Solution {
+
+    private static Set<Integer> cycleMembers =
+        new HashSet<>(Arrays.asList(4, 16, 37, 58, 89, 145, 42, 20));
+
+    public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+
+    public boolean isHappy(int n) {
+        while (n != 1 && !cycleMembers.contains(n)) {
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}
