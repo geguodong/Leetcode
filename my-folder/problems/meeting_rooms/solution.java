6efa679e8fb1ad016808312195093ecbd0efd107
geguodong <37557614+geguodong@users.noreply.github.com>
2022-02-25 21:31:03 +0000

    Sync LeetCode submission - Meeting Rooms (java)

---
+++ my-folder/problems/meeting_rooms/solution.java
@@ -0,0 +1,18 @@
+
+/*
+Sort
+Time complexity : O(nlogn). The time complexity is dominated by sorting. Once the array has been sorted, only O(n) time is taken to go through the array and determine if there is any overlap.
+
+Space complexity : O(1). Since no additional space is allocated.
+*/
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
