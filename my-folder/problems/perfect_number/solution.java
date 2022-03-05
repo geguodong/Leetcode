38b38efb76e9fd500214ea0729ea6037cb84ea35
geguodong <37557614+geguodong@users.noreply.github.com>
2022-03-05 23:13:17 +0000

    Sync LeetCode submission - Perfect Number (java)

---
+++ my-folder/problems/perfect_number/solution.java
@@ -0,0 +1,23 @@
+/*
+Time complexity : O(n). We iterate only over the range 1 < i ≤ \sqrt{num}.
+Space complexity : O(1). Constant extra space is used.
+*/
+
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+
+            }
+        }
+        return sum - num == num;
+    }
+}
