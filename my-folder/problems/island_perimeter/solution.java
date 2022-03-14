eb17ea03a9e03998e2d966b2e0c7c538bf5a5c54
geguodong <37557614+geguodong@users.noreply.github.com>
2022-03-14 05:53:47 +0000

    Sync LeetCode submission - Island Perimeter (java)

---
+++ my-folder/problems/island_perimeter/solution.java
@@ -0,0 +1,31 @@
+/*
+Time complexity : O(mn) where mm is the number of rows of the grid and nn is the number of columns of the grid. Since two for loops go through all the cells on the grid, for a two-dimensional grid of size m\times nm×n, the algorithm would have to check mnmn cells.
+
+Space complexity : O(1). Only the result variable is updated and there is no other space requirement.
+
+*/
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        int result = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    result += 4;
+                    
+                    if (r > 0 && grid[r-1][c] == 1) {
+                        result -= 2;
+                    }
+                    
+                    if (c > 0 && grid[r][c-1] == 1) {
+                        result -= 2;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
