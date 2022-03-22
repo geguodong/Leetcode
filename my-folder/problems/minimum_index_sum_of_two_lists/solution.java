1f2a0349d811602cafef3a352e533a156ecf5008
geguodong <37557614+geguodong@users.noreply.github.com>
2022-03-22 03:47:40 +0000

    Sync LeetCode submission - Minimum Index Sum of Two Lists (java)

---
+++ my-folder/problems/minimum_index_sum_of_two_lists/solution.java
@@ -0,0 +1,27 @@
+/*
+Time complexity :O(l1+l2). Every item of list2 is checked in a map of list1list1. l1 and l2 are the lengths of list1list1 and list2list2 respectively.
+
+Space complexity : O(l1∗x). hashmap size grows upto l_1∗x, where xx refers to average string length.
+*/
+
+public class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        HashMap < String, Integer > map = new HashMap < String, Integer > ();
+        for (int i = 0; i < list1.length; i++)
+            map.put(list1[i], i);
+        List < String > res = new ArrayList < > ();
+        int min_sum = Integer.MAX_VALUE, sum;
+        for (int j = 0; j < list2.length && j <= min_sum; j++) {
+            if (map.containsKey(list2[j])) {
+                sum = j + map.get(list2[j]);
+                if (sum < min_sum) {
+                    res.clear();
+                    res.add(list2[j]);
+                    min_sum = sum;
+                } else if (sum == min_sum)
+                    res.add(list2[j]);
+            }
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
