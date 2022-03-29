be989dddfd64eb1227b4c1a0f771d7265d7b305c
geguodong <37557614+geguodong@users.noreply.github.com>
2022-03-29 01:44:23 +0000

    Sync LeetCode submission - String Matching in an Array (java)

---
+++ my-folder/problems/string_matching_in_an_array/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+         Set<String> result=new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i+1; j < words.length; j++) {
+                if(words[i].contains(words[j]))
+                    result.add(words[j]) ;
+                if(words[j].contains(words[i]))
+                    result.add(words[i]) ;
+            }   
+        }
+        List<String> l1=new ArrayList<>();
+        l1.addAll(result);
+        return l1;
+    }
+}
