b7c96a62c2ea60f7b7733b2e7edc9dd2db2864f5
geguodong <37557614+geguodong@users.noreply.github.com>
2022-04-04 04:50:37 +0000

    Sync LeetCode submission - Shuffle String (java)

---
+++ my-folder/problems/shuffle_string/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] sol = new char[s.length()];
+        for(int i = 0;i<sol.length;i++){
+            sol[indices[i]] = s.charAt(i);
+        }
+        return String.valueOf(sol);
+    }
+}
