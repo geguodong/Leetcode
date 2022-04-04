fe612ba8a4a64569a482040909f9fe5334e630d8
geguodong <37557614+geguodong@users.noreply.github.com>
2022-04-04 06:12:11 +0000

    Sync LeetCode submission - Three Divisors (java)

---
+++ my-folder/problems/three_divisors/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isThree(int n) {
+        int count=0;
+        if(n==2)
+            return false;
+        for(int i=2;i<=n/2;i++)
+            if(n%i==0)
+                ++count;
+        if(count==1)
+            return true;
+        return false;
+    }
+}
