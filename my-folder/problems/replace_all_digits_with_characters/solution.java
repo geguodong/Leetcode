f751f5263e55b7a291387aee0872128759edc7d6
geguodong <37557614+geguodong@users.noreply.github.com>
2022-04-04 05:24:57 +0000

    Sync LeetCode submission - Replace All Digits with Characters (java)

---
+++ my-folder/problems/replace_all_digits_with_characters/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String replaceDigits(String s) {
+        StringBuilder s1 = new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            if(Character.isDigit(s.charAt(i)))
+            {
+                s1.append((char)(s.charAt(i-1) + Character.getNumericValue(s.charAt(i))));
+            }
+            else {
+                s1.append(s.charAt(i));
+            }
+        }
+        return s1.toString();
+    }
+}
