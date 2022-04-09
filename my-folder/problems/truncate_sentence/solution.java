a5f332fca46de6a8aa427e75d69ca1c26374ba97
geguodong <37557614+geguodong@users.noreply.github.com>
2022-04-09 05:12:29 +0000

    Sync LeetCode submission - Truncate Sentence (java)

---
+++ my-folder/problems/truncate_sentence/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int i = 0;
+        for (; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                k--;
+            }
+            
+            if (k == 0) {
+                return s.substring(0, i);
+            }
+        }
+
+        return s;
+    }
+}
