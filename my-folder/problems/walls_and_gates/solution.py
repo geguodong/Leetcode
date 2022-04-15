2c08a9f4fa78fc337123b454db5a1712b457d401
geguodong <37557614+geguodong@users.noreply.github.com>
2022-04-15 08:20:35 +0000

    Sync LeetCode submission - Walls and Gates (python3)

--- my-folder/problems/walls_and_gates/solution.py
+++ my-folder/problems/walls_and_gates/solution.py
@@ -1,21 +1,35 @@
+import collections
+
+DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        queue = deque()
-        row_len = len(rooms)
-        column_len = len(rooms[0])
-        for i in range(row_len):
-            for j in range(column_len):
+        INF = 2147483647
+        m = len(rooms)
+        n = len(rooms[0])
+        
+        queue = collections.deque()
+        for i in range(m):
+            for j in range(n):
                 if rooms[i][j] == 0:
-                    queue.append((i, j, 0))
-                
+                    queue.append((i, j))
+                                        
+        level = 1
         while queue:
-            row, column, d = queue.popleft()
-            d += 1
-            for r, c in ((row, column+1), (row, column-1), (row-1, column), (row+1, column)):
-                if r >= 0 and r < row_len and c >= 0 and c < column_len and rooms[r][c] != 0 and rooms[r][c] != -1:
-                    if d < rooms[r][c]:
-                        rooms[r][c] = d
-                        queue.append((r, c, d))
+            k = len(queue)
+            for _ in range(k):
+                curI, curJ = queue.popleft()
+                
+                for d in DIRECTIONS:
+                    newI = curI + d[0]
+                    newJ = curJ + d[1]
+                    if newI >= 0 and newI < m and newJ >= 0 and newJ < n and rooms[newI][newJ] == INF:
+                        queue.append((newI, newJ))
+                        rooms[newI][newJ] = level
+                    
+            level += 1
+            
+                
