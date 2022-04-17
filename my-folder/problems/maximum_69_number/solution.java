f508d73338820ee58e3bc4b93884cfeb247ff2d3
geguodong <37557614+geguodong@users.noreply.github.com>
2022-04-17 07:33:23 +0000

    Sync LeetCode submission - Maximum 69 Number (java)

---
+++ my-folder/problems/maximum_69_number/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximum69Number (int num) {
+        
+        char []res=String.valueOf(num).toCharArray();
+        
+        for(int i=0;i<res.length;i++)
+        {
+            if(res[i]=='6')
+            {
+                res[i]='9';
+                break;
+            }
+                
+        }
+        
+        int number = Integer.parseInt(String.valueOf(res));
+        
+        return number;
+    }
+}
