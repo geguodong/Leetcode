07ff55e1d76348e38dadc58546be7ef0a92cd9a3
geguodong <37557614+geguodong@users.noreply.github.com>
2022-04-18 06:10:22 +0000

    Sync LeetCode submission - Merge Strings Alternately (java)

---
+++ my-folder/problems/merge_strings_alternately/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int first = 0, second = 0;
+        StringBuilder sb = new StringBuilder();
+        while (first < word1.length() && second < word2.length()) {
+            sb.append(word1.charAt(first));
+            sb.append(word2.charAt(second));
+            first++;
+            second++;
+        }
+        if (first < word1.length()) {
+            sb.append(word1.substring(first));
+        }
+        if (second < word2.length()) {
+            sb.append(word2.substring(second));
+        }
+        return sb.toString();
+    }
+}
