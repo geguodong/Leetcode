66aa6b7edf4cd066284edb0e831f0717787c2e26
geguodong <37557614+geguodong@users.noreply.github.com>
2022-04-21 21:05:15 +0000

    Sync LeetCode submission - Rings and Rods (java)

---
+++ my-folder/problems/rings_and_rods/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+	public int countPoints(String rings) {
+		HashSet<Character>[]rods=new HashSet[10];
+		int count=0;
+		for(int i=0;i<=9;i++) rods[i]=new HashSet<Character>();
+		for(int i=0;i<rings.length();i+=2){
+			char c=rings.charAt(i);
+			int j=rings.charAt(i+1)-'0';
+			rods[j].add(c);
+		}
+        for(HashSet<Character> rod : rods) {
+            if (rod.size() == 3) {
+                count++;
+            }
+        }
+		return count;
+	}
+}
