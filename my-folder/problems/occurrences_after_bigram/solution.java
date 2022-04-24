67acae218a5df8633c33366b477a1d48a0ecda43
geguodong <37557614+geguodong@users.noreply.github.com>
2022-04-24 06:43:39 +0000

    Sync LeetCode submission - Occurrences After Bigram (java)

---
+++ my-folder/problems/occurrences_after_bigram/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String[] findOcurrences(String text, String first, String second) {
+        List<String> list = new ArrayList<>();
+        String[] arr = text.split(" ");
+        for(int i=0; i<arr.length-2;i++){
+            if(arr[i].equals(first) && arr[i+1].equals(second)){
+                list.add(arr[i+2]);
+            }
+        }
+        return list.toArray(String[]::new);
+    }
+}
