cc213018c4f9ee12d982db8577b1f0bc97f83c45
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-05 22:55:46 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (java)

--- my-folder/problems/binary_tree_inorder_traversal/solution.java
+++ my-folder/problems/binary_tree_inorder_traversal/solution.java
@@ -13,20 +13,18 @@
  *     }
  * }
  */
-public class Solution {
+class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
-        while (curr != null || !stack.isEmpty()) {
-            while (curr != null) {
-                stack.push(curr);
-                curr = curr.left;
-            }
-            curr = stack.pop();
-            res.add(curr.val);
-            curr = curr.right;
-        }
+        helper(root, res);
         return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            helper(root.left, res);
+            res.add(root.val);
+            helper(root.right, res);
+        }
     }
 }
