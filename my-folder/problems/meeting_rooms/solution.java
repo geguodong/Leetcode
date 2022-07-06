a03e3c703235bf93c08b870c79f40a3351f4702f
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-06 03:46:11 +0000

    Sync LeetCode submission - Meeting Rooms (java)

--- my-folder/problems/meeting_rooms/solution.java
+++ my-folder/problems/meeting_rooms/solution.java
@@ -7,7 +7,7 @@ Space complexity : O(1). Since no additional space is allocated.
 */
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         for (int i = 0; i < intervals.length - 1; i++) {
             if (intervals[i][1] > intervals[i + 1][0]) {
                 return false;
