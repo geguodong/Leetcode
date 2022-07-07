a89356b1b95c3fc923e083bd0e584017352a7f95
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-07 01:48:00 +0000

    Sync LeetCode submission - Number of Islands (java)

--- my-folder/problems/number_of_islands/solution.java
+++ my-folder/problems/number_of_islands/solution.java
@@ -1,37 +1,52 @@
-// DFS
 class Solution {
-  void dfs(char[][] grid, int r, int c) {
-    int nr = grid.length;
-    int nc = grid[0].length;
-
-    if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
-      return;
-    }
-
-    grid[r][c] = '0';
-    dfs(grid, r - 1, c);
-    dfs(grid, r + 1, c);
-    dfs(grid, r, c - 1);
-    dfs(grid, r, c + 1);
-  }
-
-  public int numIslands(char[][] grid) {
-    if (grid == null || grid.length == 0) {
-      return 0;
-    }
-
-    int nr = grid.length;
-    int nc = grid[0].length;
-    int num_islands = 0;
-    for (int r = 0; r < nr; ++r) {
-      for (int c = 0; c < nc; ++c) {
-        if (grid[r][c] == '1') {
-          ++num_islands;
-          dfs(grid, r, c);
+    int rows;
+    int cols;
+    char[][] grid;
+    int islands;
+    public int numIslands(char[][] grid) {
+        // check empty
+        if(grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+            return 0;
         }
-      }
+        
+        // initial
+        this.grid = grid;
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        this.islands = 0;
+        
+        // check nodes
+        int num = 0;
+        for(int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                bfs(i, j);
+                num = islands;
+            }
+        }
+        return islands;
     }
-
-    return num_islands;
-  }
+    
+    private void bfs(int row, int col) {
+        if(grid[row][col] == '0') {
+            return;
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        if(grid[row][col] == '1') {
+            islands++;
+            grid[row][col] = '0';
+            queue.add(new int[]{row, col});
+        }
+        int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for(int[] dir : directions) {
+                int newRow = cur[0] + dir[0];
+                int newCol = cur[1] + dir[1];
+                if(newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && grid[newRow][newCol] == '1') {
+                    grid[newRow][newCol] = '0';
+                    queue.add(new int[]{newRow, newCol});
+                }
+            }
+        }
+    }
 }
