078b7c2b4f3a4cc3ce8596d75e238e660d407e20
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-10 04:25:55 +0000

    Sync LeetCode submission - Group Anagrams (java)

---
+++ my-folder/problems/group_anagrams/solution.java
@@ -0,0 +1,23 @@
+// Count Sort + HashMap
+// O(NKlogK) O(NK) where NN is the length of strs, and KK is the maximum length of a string in strs
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}
