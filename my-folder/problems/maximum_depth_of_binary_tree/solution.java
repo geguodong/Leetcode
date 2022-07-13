179d236fc5999d7e2705de43f79f5d83d09e8cb9
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-13 18:35:28 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (java)

--- my-folder/problems/maximum_depth_of_binary_tree/solution.java
+++ my-folder/problems/maximum_depth_of_binary_tree/solution.java
@@ -19,12 +19,34 @@ Time complexity : we visit each node exactly once, thus the time complexity is O
 
 Space complexity : in the worst case, the tree is completely unbalanced, e.g. each node has only left child node, the recursion call would occur NN times (the height of the tree), therefore the storage to keep the call stack would be O(N). But in the best case (the tree is completely balanced), the height of the tree would be O(log(N)). Therefore, the space complexity in this case would be O(log(N)).
 */
-class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        if (root.left == null && root.right == null) return 1;
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+
+class Pair {
+    TreeNode key;
+    Integer value;
+    
+    public Pair(TreeNode key, Integer value) {
+        this.key = key;
+        this.value = value;
     }
-    
-    
+}
+class Solution {
+  public int maxDepth(TreeNode root) {
+     if(root == null) return 0;
+     Queue<Pair> queue = new LinkedList<>();
+     queue.add(new Pair(root, 1));
+     int maxDepth = 0;
+     while(!queue.isEmpty()) {
+         Pair pair = queue.poll();
+         TreeNode cur = pair.key;
+         int depth = pair.value;
+         maxDepth = Math.max(maxDepth, depth);
+         if(cur.left != null) {
+             queue.add(new Pair(cur.left, depth + 1));
+         }
+         if(cur.right != null) {
+             queue.add(new Pair(cur.right, depth + 1));
+         }
+     }
+     return maxDepth;
+  }
 }
