f17ef46357529cc9be543d211811cd0f0c985fd3
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-14 04:44:58 +0000

    Sync LeetCode submission - Remove Element (java)

--- my-folder/problems/remove_element/solution.java
+++ my-folder/problems/remove_element/solution.java
@@ -6,17 +6,12 @@ Space complexity : O(1).
 
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
-        int n = nums.length;
-        while (i < n) {
-            if (nums[i] == val) {
-                nums[i] = nums[n - 1];
-                // reduce array size by one
-                n--;
-            } else {
-                i++;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[count++] = nums[i];
             }
         }
-        return n;
+        return count;
     }
 }
