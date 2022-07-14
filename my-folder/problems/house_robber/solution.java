2256fe36e2f99c6e9c154c51a77eafdcb5696083
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-14 18:36:09 +0000

    Sync LeetCode submission - House Robber (java)

--- my-folder/problems/house_robber/solution.java
+++ my-folder/problems/house_robber/solution.java
@@ -1,40 +1,22 @@
-/*
-Time Complexity: O(N) since we process at most NN recursive calls, thanks to caching, and during each of these calls, we make an O(1) computation which is simply making two other recursive calls, finding their maximum, and populating the cache based on that.
-
-Space Complexity: O(N) which is occupied by the cache and also by the recursion stack.
-*/
-
+// DP only keep 2
+// O(N) O(1)
 class Solution {
-    
-    private int[] memo;
-    
     public int rob(int[] nums) {
-        
-        this.memo = new int[100];
-        
-        // Fill with sentinel value representing not-calculated recursions.
-        Arrays.fill(this.memo, -1);
-        
-        return this.robFrom(0, nums);
-    }
-    
-    private int robFrom(int i, int[] nums) {
-        
-        // No more houses left to examine.
-        if (i >= nums.length) {
+        int N = nums.length;
+        if(N == 0) {
             return 0;
         }
         
-        // Return cached value.
-        if (this.memo[i] > -1) {
-            return this.memo[i];
+        int robNext, robNextPlusOne;
+        robNextPlusOne = 0;
+        robNext = nums[N - 1];
+        
+        for(int i = N - 2; i >= 0; i--) {
+            int current = Math.max(robNext, robNextPlusOne + nums[i]);
+            
+            robNextPlusOne = robNext;
+            robNext = current;
         }
-        
-        // Recursive relation evaluation to get the optimal answer.
-        int ans = Math.max(this.robFrom(i + 1, nums), this.robFrom(i + 2, nums) + nums[i]);
-        
-        // Cache for future use.
-        this.memo[i] = ans;
-        return ans;
+        return robNext;
     }
 }
