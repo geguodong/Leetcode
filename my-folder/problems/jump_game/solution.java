bb5740d2754a403fd8bbb38bb94be7fe6e839e7a
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-17 21:37:41 +0000

    Sync LeetCode submission - Jump Game (java)

--- my-folder/problems/jump_game/solution.java
+++ my-folder/problems/jump_game/solution.java
@@ -1,28 +1,14 @@
-// DP
-// O(n^2) O(n)
-
-enum Index {
-    GOOD, BAD, UNKNOWN
-}
+// Greddy
+// O(n) O(1)
 
 public class Solution {
     public boolean canJump(int[] nums) {
-        Index[] memo = new Index[nums.length];
-        for (int i = 0; i < memo.length; i++) {
-            memo[i] = Index.UNKNOWN;
-        }
-        memo[memo.length - 1] = Index.GOOD;
-
-        for (int i = nums.length - 2; i >= 0; i--) {
-            int furthestJump = Math.min(i + nums[i], nums.length - 1);
-            for (int j = i + 1; j <= furthestJump; j++) {
-                if (memo[j] == Index.GOOD) {
-                    memo[i] = Index.GOOD;
-                    break;
-                }
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
             }
         }
-
-        return memo[0] == Index.GOOD;
+        return lastPos == 0;
     }
 }
