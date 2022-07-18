c5df55de33b2965c5574f9dd09794b2ab5cb0a70
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-18 20:48:13 +0000

    Sync LeetCode submission - Single Number II (java)

--- my-folder/problems/single_number_ii/solution.java
+++ my-folder/problems/single_number_ii/solution.java
@@ -5,12 +5,24 @@ Space complexity : O(N) to keep the set of N/3 elements.
 */
 class Solution {
   public int singleNumber(int[] nums) {
-    HashMap<Integer, Integer> hashmap = new HashMap<>();
-    for (int num : nums)
-      hashmap.put(num, hashmap.getOrDefault(num, 0) + 1);
+    int seenOnce = 0, seenTwice = 0;
 
-    for (int k : hashmap.keySet())
-      if (hashmap.get(k) == 1) return k;
-    return -1;
+    for (int num : nums) {
+      // first appearence: 
+      // add num to seen_once 
+      // don't add to seen_twice because of presence in seen_once
+
+      // second appearance: 
+      // remove num from seen_once 
+      // add num to seen_twice
+
+      // third appearance: 
+      // don't add to seen_once because of presence in seen_twice
+      // remove num from seen_twice
+      seenOnce = ~seenTwice & (seenOnce ^ num);
+      seenTwice = ~seenOnce & (seenTwice ^ num);
+    }
+
+    return seenOnce;
   }
 }
