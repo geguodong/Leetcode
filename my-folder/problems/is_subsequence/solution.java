72d96c72631f6b42e23fce45a85cf8fb572337e6
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-19 21:10:36 +0000

    Sync LeetCode submission - Is Subsequence (java)

--- my-folder/problems/is_subsequence/solution.java
+++ my-folder/problems/is_subsequence/solution.java
@@ -14,42 +14,22 @@ We built a hashmap that consists of the indices for each character in the target
 
 */
 
+// Two pointer
+// O(T)
+// O(1)
 class Solution {
 
     public boolean isSubsequence(String s, String t) {
+        Integer leftBound = s.length(), rightBound = t.length();
+        Integer pLeft = 0, pRight = 0;
 
-        // precomputation, build the hashmap out of the target string
-        HashMap<Character, List<Integer>> letterIndicesTable = new HashMap<>();
-        for (int i = 0; i < t.length(); ++i) {
-            if (letterIndicesTable.containsKey(t.charAt(i)))
-                letterIndicesTable.get(t.charAt(i)).add(i);
-            else {
-                ArrayList<Integer> indices = new ArrayList<Integer>();
-                indices.add(i);
-                letterIndicesTable.put(t.charAt(i), indices);
+        while (pLeft < leftBound && pRight < rightBound) {
+            // move both pointers or just the right pointer
+            if (s.charAt(pLeft) == t.charAt(pRight)) {
+                pLeft += 1;
             }
+            pRight += 1;
         }
-
-        Integer currMatchIndex = -1;
-        for (char letter : s.toCharArray()) {
-            if (!letterIndicesTable.containsKey(letter))
-                return false; // no match, early exist
-
-            boolean isMatched = false;
-            // greedy match with linear search
-            for (Integer matchIndex : letterIndicesTable.get(letter)) {
-                if (currMatchIndex < matchIndex) {
-                    currMatchIndex = matchIndex;
-                    isMatched = true;
-                    break;
-                }
-            }
-
-            if (!isMatched)
-                return false;
-        }
-
-        // consume all characters in the source string
-        return true;
+        return pLeft == leftBound;
     }
 }
