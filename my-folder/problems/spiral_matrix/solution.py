d5b6855d2cd04dcf125b3e5e4ecc6dcd6088017d
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-19 00:17:59 +0000

    Sync LeetCode submission - Spiral Matrix (python3)

--- my-folder/problems/spiral_matrix/solution.py
+++ my-folder/problems/spiral_matrix/solution.py
@@ -1,34 +1,24 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        n, m = len(matrix), len(matrix[0])
-        dq = []
-        if n == 1:
-            return (matrix[0][j] for j in range(m))
-        if m == 1:
-            return (matrix[i][0] for i in range(n))
-        startset, sizen, sizem = 0, n, m
-        while len(dq) < m * n:
-            for i in range(sizem - 1):
-                dq.append(matrix[startset][startset+i])
-            for j in range(sizen - 1):
-                dq.append(matrix[startset+j][startset+sizem-1])
-            for k in range(sizem - 1):
-                dq.append(matrix[startset+sizen-1][startset+sizem-1-k])
-            for l in range(sizen - 1):
-                dq.append(matrix[startset+sizen-1-l][startset])
-            startset += 1
-            sizem -= 2
-            sizen -= 2
-            if sizem == sizen == 1:
-                dq.append(matrix[startset][startset])
-            elif sizem == 1:
-                for i in range(sizen):
-                    dq.append(matrix[startset+i][startset])
-            elif sizen == 1:
-                for i in range(sizem):
-                    dq.append(matrix[startset][startset+i])
-        return dq
-        
-            
-        
-        
+        if matrix is None or len(matrix) is None or len(matrix[0]) is None:
+            return None
+        result = []
+        rows, cols = len(matrix), len(matrix[0])
+        left, right, up, down = 0, cols - 1, 0, rows - 1
+        while len(result) < rows * cols:
+            for col in range(left, right + 1):
+                result.append(matrix[up][col])
+            for row in range(up + 1, down + 1):
+                result.append(matrix[row][right])
+            if up != down:
+                for col in range(right - 1, left - 1, -1):
+                    result.append(matrix[down][col])
+            if left != right:
+                for row in range(down - 1, up, -1):
+                    result.append(matrix[row][left])
+                    
+            left += 1
+            right -= 1
+            up += 1
+            down -= 1
+        return result
