c3cab22a3a4f0536829c0a5ba78b869ba175a09b
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-20 18:56:31 +0000

    Sync LeetCode submission - Valid Palindrome II (java)

--- my-folder/problems/valid_palindrome_ii/solution.java
+++ my-folder/problems/valid_palindrome_ii/solution.java
@@ -1,11 +1,12 @@
 /*
+Two Pointer
 O（n）
 */
+
 class Solution {
     public static boolean validPalindrome(String s) {
         int i = 0;
         int j = s.length() - 1;
-        boolean isRemoved = false;
         while (i < j) {
             if (s.charAt(i) != s.charAt(j)) {
                 return isPal(s, i + 1, j) || isPal(s, i, j - 1);
