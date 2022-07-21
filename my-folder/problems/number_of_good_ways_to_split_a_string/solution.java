caaeab718f31a0e7328772638d1a9d586265e2c0
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-21 20:48:38 +0000

    Sync LeetCode submission - Number of Good Ways to Split a String (java)

--- my-folder/problems/number_of_good_ways_to_split_a_string/solution.java
+++ my-folder/problems/number_of_good_ways_to_split_a_string/solution.java
@@ -1,41 +1,30 @@
 // O(n) O(n)
 public class Solution {
     public int numSplits(String s) {
-        int length = s.length();
-        if(length < 2)
+        if(s == null || s.length() < 2) {
             return 0;
-
-         if(length == 2)
-            if(s.charAt(0) == s.charAt(1))
-                return 1;
-
-        HashMap<Character, Integer> hm = new HashMap<>();
-        HashMap<Character, Integer> hm2 = new HashMap<>();
-
-        //set up 2 hashmaps
-        hm.put(s.charAt(0), 1);
-        for(int i = 1; i < length; ++i)
-            hm2.put(s.charAt(i), hm2.getOrDefault(s.charAt(i), 0) + 1);
-
-        //check if the above set up is a good way
-        int count = 0;
-        if(hm2.size() == hm.size())
-            ++count;
-
-        //use a pointer and adjust the count of character occurences in the 2 hashmaps accordingly.
-        for(int i = 1; i < s.length()-1; ++i)
-        {
-            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
-            int temp = hm2.get(s.charAt(i))-1;
-            if(temp > 0)
-                hm2.put(s.charAt(i), temp);
-            else
-                hm2.remove(s.charAt(i));
-
-            if(hm2.size() == hm.size()) //same number of distinct characters
-                ++count;
         }
-
+        
+        // add char to map
+        Map<Character, Integer> rightMap = new HashMap<>();
+        for(char c : s.toCharArray()) {
+            rightMap.put(c, rightMap.getOrDefault(c, 0) + 1);
+        }
+        
+        int count = 0; 
+        // traverse
+        Map<Character, Integer> leftMap = new HashMap<>();
+        for(int i = 0; i < s.length() - 1; i++) {
+            char c = s.charAt(i);
+            leftMap.put(c, leftMap.getOrDefault(c, 0) + 1);
+            rightMap.put(c, rightMap.get(c) - 1);
+            if(rightMap.get(c) == 0) {
+                rightMap.remove(c);
+            }
+            if(leftMap.size() == rightMap.size()) {
+                count++;
+            }
+        }
         return count;
     }
     
