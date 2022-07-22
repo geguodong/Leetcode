de04a557fca117ebd30e6977b7c0791ecdb6bb5d
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-22 04:17:02 +0000

    Sync LeetCode submission - Make Two Arrays Equal by Reversing Subarrays (java)

--- my-folder/problems/make_two_arrays_equal_by_reversing_subarrays/solution.java
+++ my-folder/problems/make_two_arrays_equal_by_reversing_subarrays/solution.java
@@ -1,9 +1,18 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i:target) map.put(i, map.getOrDefault(i, 0) + 1);
-        for(int i:arr) map.put(i, map.getOrDefault(i, 0) - 1);
-        for(int key: map.keySet())if (map.get(key) != 0) return false;
+        for(int i:target) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        for(int i:arr) {
+            map.put(i, map.getOrDefault(i, 0) - 1);
+        }
+        for(int key: map.keySet()) {
+            if (map.get(key) != 0) {
+                return false;
+            }
+        }
+            
         return true;
     }
 }
