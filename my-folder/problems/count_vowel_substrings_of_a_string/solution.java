5c1f8b8005a015c708594f9748295b841e830f37
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-22 06:42:22 +0000

    Sync LeetCode submission - Count Vowel Substrings of a String (java)

--- my-folder/problems/count_vowel_substrings_of_a_string/solution.java
+++ my-folder/problems/count_vowel_substrings_of_a_string/solution.java
@@ -3,7 +3,7 @@ class Solution {
         int ans=0;
         for(int i=0;i<word.length();i++) {
             HashMap<Character,Integer> memo = new HashMap<>();
-            for(int j=i;j<word.length();j++) {                                                    if(word.charAt(j)=='a'||word.charAt(j)=='e'||word.charAt(j)=='i'||word.charAt(j)=='o'||word.charAt(j)=='u') {
+            for(int j=i;j<word.length();j++) {                                                                  if(word.charAt(j)=='a'||word.charAt(j)=='e'||word.charAt(j)=='i'||word.charAt(j)=='o'||word.charAt(j)=='u') {
                 memo.put(word.charAt(j),1);
                 if(memo.size()==5){
                     ans+=1;
