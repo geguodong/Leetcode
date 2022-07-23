77b14d246853238bcb657d273795ab8e6044b03f
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-23 05:35:07 +0000

    Sync LeetCode submission - Maximum Product of Two Elements in an Array (java)

--- my-folder/problems/maximum_product_of_two_elements_in_an_array/solution.java
+++ my-folder/problems/maximum_product_of_two_elements_in_an_array/solution.java
@@ -1,18 +1,15 @@
-/*
-O(n)
-O(n)
-Note : also could use sort
-*/
-
 class Solution {
     public int maxProduct(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int val: nums){
-            pq.add(val);
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for(int num : nums) {
+            if(num > first) {
+                second = first;
+                first = num;
+            } else if(num > second) {
+                second = num;
+            }
         }
-        int a = pq.peek()-1;
-        pq.remove();
-        int b = pq.peek()-1;
-        return a*b;
+        return (first - 1) * (second - 1);
     }
 }
