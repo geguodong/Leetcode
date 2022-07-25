07a4f01749fce8e1348f41c1f339b66b265466ae
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-25 21:20:05 +0000

    Sync LeetCode submission - Middle of the Linked List (java)

--- my-folder/problems/middle_of_the_linked_list/solution.java
+++ my-folder/problems/middle_of_the_linked_list/solution.java
@@ -8,13 +8,18 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// 如果链表长度为偶数，也就是说中点有两个的时候，我们这个解法返回的节点是靠后的那个节点。
 class Solution {
-    public ListNode middleNode(ListNode head) {
+    ListNode middleNode(ListNode head) {
+        // 快慢指针初始化指向 head
         ListNode slow = head, fast = head;
+        // 快指针走到末尾时停止
         while (fast != null && fast.next != null) {
+            // 慢指针走一步，快指针走两步
             slow = slow.next;
             fast = fast.next.next;
         }
+        // 慢指针指向中点
         return slow;
     }
 }
