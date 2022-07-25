87f554fbe78226dd1ab6c7236385d7f4beea776a
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-25 21:16:57 +0000

    Sync LeetCode submission - Remove Nth Node From End of List (java)

--- my-folder/problems/remove_nth_node_from_end_of_list/solution.java
+++ my-folder/problems/remove_nth_node_from_end_of_list/solution.java
@@ -9,21 +9,31 @@
  * }
  */
 class Solution {
+    // 主函数
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode fast = dummy;
-        ListNode slow = dummy;
-        // Advances first pointer so that the gap between first and second is n nodes apart
-        for (int i = 0; i <= n; i++) {
-            fast = fast.next;
-        }
-        // Move first to the end, maintaining the gap
-        while (fast != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-        slow.next = slow.next.next;
+        // 删除倒数第 n 个，要先找倒数第 n + 1 个节点
+        ListNode x = findFromEnd(dummy, n + 1);
+        // 删掉倒数第 n 个节点
+        x.next = x.next.next;
         return dummy.next;
+    }
+
+    ListNode findFromEnd(ListNode head, int k) {
+        ListNode p1 = head;
+        // p1 先走 k 步
+        for (int i = 0; i < k; i++) {
+            p1 = p1.next;
+        }
+        ListNode p2 = head;
+        // p1 和 p2 同时走 n - k 步
+        while (p1 != null) {
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+        // p2 现在指向第 n - k + 1 个节点，即倒数第 k 个节点
+        return p2;
     }
 }
