3b230fb14487c1f0014c76c003a8c4ba00f5823b
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-25 21:25:38 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (java)

--- my-folder/problems/intersection_of_two_linked_lists/solution.java
+++ my-folder/problems/intersection_of_two_linked_lists/solution.java
@@ -10,17 +10,17 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode pA = headA;
-        ListNode pB = headB;
-        while (pA != pB) {
-            pA = pA == null ? headB : pA.next;
-            pB = pB == null ? headA : pB.next;
+    ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // p1 指向 A 链表头结点，p2 指向 B 链表头结点
+        ListNode p1 = headA, p2 = headB;
+        while (p1 != p2) {
+            // p1 走一步，如果走到 A 链表末尾，转到 B 链表
+            if (p1 == null) p1 = headB;
+            else            p1 = p1.next;
+            // p2 走一步，如果走到 B 链表末尾，转到 A 链表
+            if (p2 == null) p2 = headA;
+            else            p2 = p2.next;
         }
-        return pA;
-        // Note: In the case lists do not intersect, the pointers for A and B
-        // will still line up in the 2nd iteration, just that here won't be
-        // a common node down the list and both will reach their respective ends
-        // at the same time. So pA will be NULL in that case.
+        return p1;
     }
 }
