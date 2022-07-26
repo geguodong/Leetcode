e4a1485fc84f301e99d25a4700e2a9a21f0d4c3a
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-26 07:08:11 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (java)

--- my-folder/problems/longest_substring_without_repeating_characters/solution.java
+++ my-folder/problems/longest_substring_without_repeating_characters/solution.java
@@ -1,36 +1,29 @@
-/*
-Sliding Window Optimized
-Time complexity : O(n). Index jj will iterate nn times.
-Space complexity (HashMap) : O(min(m,n)). Same as the previous approach.
-Space complexity (Table): O(m). mm is the size of the charset.
-int[26] for Letters 'a' - 'z' or 'A' - 'Z'
-int[128] for ASCII
-int[256] for Extended ASCII
-*/
-
-
-public class Solution {
+class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Integer[] chars = new Integer[128];
-
-        int left = 0;
-        int right = 0;
-
+        HashMap<Character,Integer> window = new HashMap<>();
+        int left = 0, right = 0;
         int res = 0;
-        while (right < s.length()) {
-            char r = s.charAt(right);
-
-            Integer index = chars[r];
-            if (index != null && index >= left && index < right) {
-                left = index + 1;
-            }
-
-            res = Math.max(res, right - left + 1);
-
-            chars[r] = right;
+        while (right < s.length()) {          
+            // c 是移入窗口的字符
+            char c = s.charAt(right);
+            // 窗口右移
             right++;
-        }
+            // 更新窗口的一系列数据
+            window.put(c, window.getOrDefault(c, 0) + 1);
 
+                // 判断左侧窗口是否需要收缩
+                while (window.get(c) > 1) {
+                    // d 是移出窗口的字符
+                    char d = s.charAt(left);
+                    // 窗口右移
+                    left++;
+                    // 更新窗口的一系列数据
+                    window.put(d, window.get(d) - 1);
+                }
+                // 这里更新最长子串的长度
+                int len = right - left;
+                res = Math.max(res, len);
+        }
         return res;
     }
 }
