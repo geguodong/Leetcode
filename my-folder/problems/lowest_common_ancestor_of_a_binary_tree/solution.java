c12e53274d2fc477968f246a8f3546b181f718a0
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-27 05:12:39 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (java)

--- my-folder/problems/lowest_common_ancestor_of_a_binary_tree/solution.java
+++ my-folder/problems/lowest_common_ancestor_of_a_binary_tree/solution.java
@@ -7,47 +7,29 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-// Recursion
-// O(N) O(N)
 class Solution {
-
-    private TreeNode ans;
-
-    public Solution() {
-        // Variable to store LCA node.
-        this.ans = null;
+    TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return find(root, p.val, q.val);
     }
 
-    private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
-
-        // If reached the end of a branch, return false.
-        if (currentNode == null) {
-            return false;
+    // 在二叉树中寻找 val1 和 val2 的最近公共祖先节点
+    TreeNode find(TreeNode root, int val1, int val2) {
+        if (root == null) {
+            return null;
+        }
+        // 前序位置
+        if (root.val == val1 || root.val == val2) {
+            // 如果遇到目标值，直接返回
+            return root;
+        }
+        TreeNode left = find(root.left, val1, val2);
+        TreeNode right = find(root.right, val1, val2);
+        // 后序位置，已经知道左右子树是否存在目标值
+        if (left != null && right != null) {
+            // 当前节点是 LCA 节点
+            return root;
         }
 
-        // Left Recursion. If left recursion returns true, set left = 1 else 0
-        int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
-
-        // Right Recursion
-        int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
-
-        // If the current node is one of p or q
-        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
-
-
-        // If any two of the flags left, right or mid become True
-        if (mid + left + right >= 2) {
-            this.ans = currentNode;
-        }
-
-        // Return true if any one of the three bool values is True.
-        return (mid + left + right > 0);
-    }
-
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // Traverse the tree
-        this.recurseTree(root, p, q);
-        return this.ans;
+        return left != null ? left : right;
     }
 }
