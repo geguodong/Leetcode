8595609b2bb95a9325924a4721ceaa288f3d4275
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-27 16:42:28 +0000

    Sync LeetCode submission - Permutations (java)

--- my-folder/problems/permutations/solution.java
+++ my-folder/problems/permutations/solution.java
@@ -1,36 +1,45 @@
-// BackTracking
-// O(N×N!) and a bit slower than \mathcal{O}(N!)O(N!).
-// O(N!) since one has to keep N! solutions.
 class Solution {
-  public void backtrack(int n,
-                        ArrayList<Integer> nums,
-                        List<List<Integer>> output,
-                        int first) {
-    // if all integers are used up
-    if (first == n)
-      output.add(new ArrayList<Integer>(nums));
-    for (int i = first; i < n; i++) {
-      // place i-th integer first 
-      // in the current permutation
-      Collections.swap(nums, first, i);
-      // use next integers to complete the permutations
-      backtrack(n, nums, output, first + 1);
-      // backtrack
-      Collections.swap(nums, first, i);
+    
+
+/* 主函数，输入一组不重复的数字，返回它们的全排列 */
+    List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+    // 记录「路径」
+        LinkedList<Integer> track = new LinkedList<>();
+    // 「路径」中的元素会被标记为 true，避免重复使用
+        boolean[] used = new boolean[nums.length];
+    
+        backtrack(nums, track, used, res);
+        return res;
     }
-  }
 
-  public List<List<Integer>> permute(int[] nums) {
-    // init output list
-    List<List<Integer>> output = new LinkedList();
-
-    // convert nums into list since the output is a list of lists
-    ArrayList<Integer> nums_lst = new ArrayList<Integer>();
-    for (int num : nums)
-      nums_lst.add(num);
-
-    int n = nums.length;
-    backtrack(n, nums_lst, output, 0);
-    return output;
-  }
+// 路径：记录在 track 中
+// 选择列表：nums 中不存在于 track 的那些元素（used[i] 为 false）
+// 结束条件：nums 中的元素全都在 track 中出现
+    void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used, List<List<Integer>> res) {
+    // 触发结束条件
+        if (track.size() == nums.length) {
+            res.add(new LinkedList(track));
+            return;
+        }
+    
+        for (int i = 0; i < nums.length; i++) {
+            // 排除不合法的选择
+            if (used[i]) {
+                // nums[i] 已经在 track 中，跳过
+                continue;
+            }
+            // 做选择
+            track.add(nums[i]);
+            used[i] = true;
+            // 进入下一层决策树
+            backtrack(nums, track, used, res);
+            // 取消选择
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+    
 }
+
+
