26f827d90fc0cc77d9e14e2f0dbbfe802983bf1c
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-27 05:21:43 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Search Tree (java)

--- my-folder/problems/lowest_common_ancestor_of_a_binary_search_tree/solution.java
+++ my-folder/problems/lowest_common_ancestor_of_a_binary_search_tree/solution.java
@@ -9,26 +9,28 @@
  */
 
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 保证 val1 较小，val2 较大
+        int val1 = Math.min(p.val, q.val);
+        int val2 = Math.max(p.val, q.val);
+        return find(root, val1, val2);
+    }
 
-        // Value of current node or parent node.
-        int parentVal = root.val;
-
-        // Value of p
-        int pVal = p.val;
-
-        // Value of q;
-        int qVal = q.val;
-
-        if (pVal > parentVal && qVal > parentVal) {
-            // If both p and q are greater than parent
-            return lowestCommonAncestor(root.right, p, q);
-        } else if (pVal < parentVal && qVal < parentVal) {
-            // If both p and q are lesser than parent
-            return lowestCommonAncestor(root.left, p, q);
-        } else {
-            // We have found the split point, i.e. the LCA node.
-            return root;
+    // 在 BST 中寻找 val1 和 val2 的最近公共祖先节点
+    TreeNode find(TreeNode root, int val1, int val2) {
+        if (root == null) {
+            return null;
         }
+        if (root.val > val2) {
+            // 当前节点太大，去左子树找
+            return find(root.left, val1, val2);
+        }
+        if (root.val < val1) {
+            // 当前节点太小，去右子树找
+            return find(root.right, val1, val2);
+        }
+        // val1 <= root.val <= val2
+        // 则当前节点就是最近公共祖先
+        return root;
     }
 }
