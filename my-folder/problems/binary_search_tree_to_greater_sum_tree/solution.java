f3463536e396200758a07c9cc5d4bea9d4ce8493
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-27 03:57:46 +0000

    Sync LeetCode submission - Binary Search Tree to Greater Sum Tree (java)

---
+++ my-folder/problems/binary_search_tree_to_greater_sum_tree/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {  
+    
+    TreeNode bstToGst(TreeNode root) {
+    traverse(root);
+    return root;
+}
+
+// 记录累加和
+int sum = 0;
+void traverse(TreeNode root) {
+    if (root == null) {
+        return;
+    }
+    traverse(root.right);
+    // 维护累加和
+    sum += root.val;
+    // 将 BST 转化成累加树
+    root.val = sum;
+    traverse(root.left);
+}
+    
+    
+}
