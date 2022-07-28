84a83e7bedc0b067697f6127b716b5a467d6174c
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-28 23:34:06 +0000

    Sync LeetCode submission - Combination Sum (java)

--- my-folder/problems/combination_sum/solution.java
+++ my-folder/problems/combination_sum/solution.java
@@ -1,28 +1,32 @@
-// BackTracking
-// Let N be the number of candidates, T  be the target value, and M be the minimal value among the candidates.
-// Time Complexity: O(N^ T/M+1)
-// Space Complexity: O(N^ T/M)   
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        helper(candidates, 0, target, res, cur);
+        LinkedList<Integer> path = new LinkedList<>();
+        if(candidates.length == 0) {
+            return res;
+        }
+        backtrack(candidates, 0,  res, path, 0, target);
         return res;
     }
     
-    private void helper(int[] candidates, int start, int target, List<List<Integer>> res, List<Integer> cur) {
-        if(target < 0) return;
-        if(target == 0) {
-            res.add(new ArrayList<Integer>(cur));
-        }
-
-        for (int i = start; i < candidates.length; ++i) {
-            // add the number into the combination
-            cur.add(candidates[i]);
-            helper(candidates, i, target - candidates[i], res, cur);
-            // backtrack, remove the number from the combination
-            cur.remove(cur.size() - 1);
+    private void backtrack(int[] candidates, int start, List<List<Integer>> res, LinkedList<Integer> path, int curSum, int target) {
+        if(curSum == target) {
+            res.add(new LinkedList<>(path));
+            return;
         }
         
-    }
+        if(curSum > target) {
+            return;
+        }
+        
+        for(int i = start; i < candidates.length; i++) {
+            curSum += candidates[i];
+            path.add(candidates[i]);
+            backtrack(candidates, i, res, path, curSum, target);
+            curSum -= candidates[i];
+            path.removeLast();
+        }
+    } 
+    
 }
