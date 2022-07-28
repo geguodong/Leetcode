7a5df99ff2ad3ea560e5bb5545de681e3eac1719
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-28 03:48:42 +0000

    Sync LeetCode submission - Meeting Rooms II (java)

--- my-folder/problems/meeting_rooms_ii/solution.java
+++ my-folder/problems/meeting_rooms_ii/solution.java
@@ -1,51 +1,33 @@
-// O(Nlogn) O(n)
 class Solution {
-    public int minMeetingRooms(int[][] intervals) {
-        
-    // Check for the base case. If there are no intervals, return 0
-    if (intervals.length == 0) {
-      return 0;
+    int minMeetingRooms(int[][] meetings) {
+        int n = meetings.length;
+        int[] begin = new int[n];
+        int[] end = new int[n];
+        for(int i = 0; i < n; i++) {
+            begin[i] = meetings[i][0];
+            end[i] = meetings[i][1];
+        }
+        Arrays.sort(begin);
+        Arrays.sort(end);
+
+        // 扫描过程中的计数器
+        int count = 0;
+        // 双指针技巧
+        int res = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (begin[i] < end[j]) {
+                // 扫描到一个红点
+                count++;
+                i++;
+            } else {
+                // 扫描到一个绿点
+                count--;
+                j++;
+            }
+            // 记录扫描过程中的最大值
+            res = Math.max(res, count);
+        }
+
+        return res;
     }
-
-    // Min heap
-    PriorityQueue<Integer> allocator =
-        new PriorityQueue<Integer>(
-            intervals.length,
-            (a, b) -> a - b
-            // new Comparator<Integer>() {
-            //   public int compare(Integer a, Integer b) {
-            //     return a - b;
-            //   }
-          //  }
-    );
-
-    // Sort the intervals by start time
-    Arrays.sort(
-        intervals, (a, b) -> a[0] - b[0]
-        // new Comparator<int[]>() {
-        //   public int compare(final int[] a, final int[] b) {
-        //     return a[0] - b[0];
-        //   }
-      //  }
-    );
-
-    // Add the first meeting
-    allocator.add(intervals[0][1]);
-
-    // Iterate over remaining intervals
-    for (int i = 1; i < intervals.length; i++) {
-
-      // If the room due to free up the earliest is free, assign that room to this meeting.
-      if (intervals[i][0] >= allocator.peek()) {
-        allocator.poll();
-      }
-
-      // If a new room is to be assigned, then also we add to the heap,
-      // If an old room is allocated, then also we have to add to the heap with updated end time.
-      allocator.add(intervals[i][1]);
-    }
-
-    // The size of the heap tells us the minimum rooms required for all the meetings.
-    return allocator.size();
-  }
 }
