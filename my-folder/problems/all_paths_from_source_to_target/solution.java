fb53ea53ba8d5398b8b3c73d69d425d161ebefc3
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-28 05:51:25 +0000

    Sync LeetCode submission - All Paths From Source to Target (java)

--- my-folder/problems/all_paths_from_source_to_target/solution.java
+++ my-folder/problems/all_paths_from_source_to_target/solution.java
@@ -1,70 +1,35 @@
-/*
-Time Complexity: O(2^N ⋅N)
-
-As we calculate shortly before, there could be at most 2^{N-1} - 12 
-N−1
- −1 possible paths in the graph.
-
-For each path, there could be at most N-2N−2 intermediate nodes, i.e. it takes \mathcal{O}(N)O(N) time to build a path.
-
-To sum up, a loose upper-bound on the time complexity of the algorithm would be (2^{N-1} - 1) \cdot \mathcal{O}(N) = \mathcal{O}(2^N \cdot N)(2 
-N−1
- −1)⋅O(N)=O(2 
-N
- ⋅N), where we consider it takes \mathcal{O}(N)O(N) efforts to build each path.
-
-It is a loose uppper bound, since we could have overlapping among the paths, therefore the efforts to build certain paths could benefit others.
-
-Space Complexity: Time Complexity: O(2^N ⋅N)
-
-Similarly, since at most we could have 2^{N-1}-12 
-N−1
- −1 paths as the results and each path can contain up to NN nodes, the space we need to store the results would be \mathcal{O}(2^N \cdot N)O(2 
-N
- ⋅N).
-
-Since we also applied recursion in the algorithm, the recursion could incur additional memory consumption in the function call stack. The stack can grow up to NN consecutive calls. Meanwhile, along with the recursive call, we also keep the state of the current path, which could take another \mathcal{O}(N)O(N) space. Therefore, in total, the recursion would require additional \mathcal{O}(N)O(N) space.
-
-To sum up, the space complexity of the algorithm is \mathcal{O}(2^N \cdot N) + \mathcal{O}(N) = \mathcal{O}(2^N \cdot N)O(2 
-N
- ⋅N)+O(N)=O(2 
-N
- ⋅N).
-
-*/
-
-// back tracking
 class Solution {
-    private int target;
-    private int[][] graph;
-    private List<List<Integer>> results;
-
-    protected void backtrack(int currNode, LinkedList<Integer> path) {
-        if (currNode == this.target) {
-            // Note: one should make a deep copy of the path
-            this.results.add(new ArrayList<Integer>(path));
-            return;
-        }
-        // explore the neighbor nodes one after another.
-        for (int nextNode : this.graph[currNode]) {
-            // mark the choice, before backtracking.
-            path.addLast(nextNode);
-            this.backtrack(nextNode, path);
-            // remove the previous choice, to try the next choice
-            path.removeLast();
-        }
-    }
+    // 记录所有路径
+    List<List<Integer>> res = new LinkedList<>();
 
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        // 维护递归过程中经过的路径
+        LinkedList<Integer> path = new LinkedList<>();
+        traverse(graph, 0, path);
+        return res;
+    }
 
-        this.target = graph.length - 1;
-        this.graph = graph;
-        this.results = new ArrayList<List<Integer>>();
-        // adopt the LinkedList for fast access to the tail element.
-        LinkedList<Integer> path = new LinkedList<Integer>();
-        path.addLast(0);
-        // kick of the backtracking, starting from the source (node 0)
-        this.backtrack(0, path);
-        return this.results;
+    /* 图的遍历框架 */
+    void traverse(int[][] graph, int s, LinkedList<Integer> path) {
+        // 添加节点 s 到路径
+        path.addLast(s);
+
+        int n = graph.length;
+        if (s == n - 1) {
+            // 到达终点
+            res.add(new LinkedList<>(path));
+            // 可以在这直接 return，但要 removeLast 正确维护 path
+            // path.removeLast();
+            // return;
+            // 不 return 也可以，因为图中不包含环，不会出现无限递归
+        }
+
+        // 递归每个相邻节点
+        for (int v : graph[s]) {
+            traverse(graph, v, path);
+        }
+
+        // 从路径移出节点 s
+        path.removeLast();
     }
 }
