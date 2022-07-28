f11d5d999d96bde96787580e2d987ee1d3e653ab
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-28 15:08:23 +0000

    Sync LeetCode submission - Find the Celebrity (java)

--- my-folder/problems/find_the_celebrity/solution.java
+++ my-folder/problems/find_the_celebrity/solution.java
@@ -1,42 +1,31 @@
 /* The knows API is defined in the parent class Relation.
       boolean knows(int a, int b); */
 
-// Logical Deduction with cache
-// O(N) O(n)
 public class Solution extends Relation {
-    
-    private int numberOfPeople;
-    private Map<Pair<Integer, Integer>, Boolean> cache = new HashMap<>(); 
-    
-    @Override
-    public boolean knows(int a, int b) {
-        if (!cache.containsKey(new Pair(a, b))) {
-            cache.put(new Pair(a, b), super.knows(a, b));
-        }
-        return cache.get(new Pair(a, b));
-    }
-    
-    public int findCelebrity(int n) {
-        numberOfPeople = n;
-        int celebrityCandidate = 0;
-        for (int i = 0; i < n; i++) {
-            if (knows(celebrityCandidate, i)) {
-                celebrityCandidate = i;
+   public int findCelebrity(int n) {
+        // 先假设 cand 是名人
+        int cand = 0;
+        for (int other = 1; other < n; other++) {
+            if (!knows(other, cand) || knows(cand, other)) {
+                // cand 不可能是名人，排除
+                // 假设 other 是名人
+                cand = other;
+            } else {
+                // other 不可能是名人，排除
+                // 什么都不用做，继续假设 cand 是名人
             }
         }
-        if (isCelebrity(celebrityCandidate)) {
-            return celebrityCandidate;
-        }
-        return -1;
-    }
-    
-    private boolean isCelebrity(int i) {
-        for (int j = 0; j < numberOfPeople; j++) {
-            if (i == j) continue; // Don't ask if they know themselves.
-            if (knows(i, j) || !knows(j, i)) {
-                return false;
+
+        // 现在的 cand 是排除的最后结果，但不能保证一定是名人
+        for (int other = 0; other < n; other++) {
+            if (cand == other) continue;
+            // 需要保证其他人都认识 cand，且 cand 不认识任何其他人
+            if (!knows(other, cand) || knows(cand, other)) {
+                return -1;
             }
         }
-        return true;
-    }
+
+        return cand;
+    } 
+
 }
