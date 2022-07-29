73c7843586dde7164f6c2e0062f6633c5498f0b5
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-29 17:46:05 +0000

    Sync LeetCode submission - Minimum Depth of Binary Tree (java)

--- my-folder/problems/minimum_depth_of_binary_tree/solution.java
+++ my-folder/problems/minimum_depth_of_binary_tree/solution.java
@@ -14,38 +14,29 @@
  * }
  */
 
-/*
-DFS
-Time complexity : each node is visited exactly once and time complexity is \mathcal{O}(N)O(N).
-
-Space complexity : in the worst case we could keep up to the entire tree, that results in \mathcal{O}(N)O(N) space complexity.
-
-*/
 class Solution {
-  public int minDepth(TreeNode root) {
-    LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>();
-    if (root == null) {
-      return 0;
+    public int minDepth(TreeNode root) {
+        LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+        if (root == null) {
+            return 0;
+        } else {
+            stack.add(new Pair(root, 1));
+        }
+        int min_depth = Integer.MAX_VALUE;
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> current = stack.pollLast();
+            root = current.getKey();
+            int current_depth = current.getValue();
+            if ((root.left == null) && (root.right == null)) {
+                min_depth = Math.min(min_depth, current_depth);
+            }
+            if (root.left != null) {
+                stack.add(new Pair(root.left, current_depth + 1));
+            }
+            if (root.right != null) {
+                stack.add(new Pair(root.right, current_depth + 1));
+            }
+        }
+        return min_depth;
     }
-    else {
-      stack.add(new Pair(root, 1));
-    }
-
-    int min_depth = Integer.MAX_VALUE;
-    while (!stack.isEmpty()) {
-      Pair<TreeNode, Integer> current = stack.pollLast();
-      root = current.getKey();
-      int current_depth = current.getValue();
-      if ((root.left == null) && (root.right == null)) {
-        min_depth = Math.min(min_depth, current_depth);
-      }
-      if (root.left != null) {
-        stack.add(new Pair(root.left, current_depth + 1));
-      }
-      if (root.right != null) {
-        stack.add(new Pair(root.right, current_depth + 1));
-      }
-    }
-    return min_depth;
-  }
 }
