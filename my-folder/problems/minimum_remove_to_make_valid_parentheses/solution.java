027fef5c566db328b3a3b1ab3d04faa007fe67e0
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-29 22:52:08 +0000

    Sync LeetCode submission - Minimum Remove to Make Valid Parentheses (java)

--- my-folder/problems/minimum_remove_to_make_valid_parentheses/solution.java
+++ my-folder/problems/minimum_remove_to_make_valid_parentheses/solution.java
@@ -1,29 +1,28 @@
-/*
- Two Pass String Builder
-
-*/
-
 class Solution {
-
-    private StringBuilder removeInvalidClosing(CharSequence string, char open, char close) {
-        StringBuilder sb = new StringBuilder();
-        int balance = 0;
-        for (int i = 0; i < string.length(); i++) {
-            char c = string.charAt(i);
-            if (c == open) {
-                balance++;
-            } if (c == close) {
-                if (balance == 0) continue;
-                balance--;
-            }
-            sb.append(c);
-        }  
-        return sb;
-    }
-
     public String minRemoveToMakeValid(String s) {
-        StringBuilder result = removeInvalidClosing(s, '(', ')');
-        result = removeInvalidClosing(result.reverse(), ')', '(');
-        return result.reverse().toString();
+        Set<Integer> indexesToRemove = new HashSet<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+             } 
+            if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    indexesToRemove.add(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        // Put any indexes remaining on stack into the set.
+        while (!stack.isEmpty()) indexesToRemove.add(stack.pop());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexesToRemove.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
+
