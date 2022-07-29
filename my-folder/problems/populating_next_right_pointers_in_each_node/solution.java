f5815aea810010bede3e04b8492f7d30b75d51a5
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-29 18:32:28 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (java)

--- my-folder/problems/populating_next_right_pointers_in_each_node/solution.java
+++ my-folder/problems/populating_next_right_pointers_in_each_node/solution.java
@@ -21,45 +21,28 @@ class Node {
 };
 */
 
-// Using previously established next pointers
-// O(N) O(1)
 class Solution {
-    public Node connect(Node root) {
-        
-        if (root == null) {
-            return root;
-        }
-        
-        // Start with the root node. There are no next pointers
-        // that need to be set up on the first level
-        Node leftmost = root;
-        
-        // Once we reach the final level, we are done
-        while (leftmost.left != null) {
-            
-            // Iterate the "linked list" starting from the head
-            // node and using the next pointers, establish the 
-            // corresponding links for the next level
-            Node head = leftmost;
-            
-            while (head != null) {
-                
-                // CONNECTION 1
-                head.left.next = head.right;
-                
-                // CONNECTION 2
-                if (head.next != null) {
-                    head.right.next = head.next.left;
-                }
-                
-                // Progress along the list (nodes on the current level)
-                head = head.next;
-            }
-            
-            // Move onto the next level
-            leftmost = leftmost.left;
-        }
-        
+    // 主函数
+    Node connect(Node root) {
+        if (root == null) return null;
+        // 遍历「三叉树」，连接相邻节点
+        traverse(root.left, root.right);
         return root;
+    }
+
+    // 三叉树遍历框架
+    void traverse(Node node1, Node node2) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        /**** 前序位置 ****/
+        // 将传入的两个节点穿起来
+        node1.next = node2;
+
+        // 连接相同父节点的两个子节点
+        traverse(node1.left, node1.right);
+        traverse(node2.left, node2.right);
+        // 连接跨越父节点的两个子节点
+        traverse(node1.right, node2.left);
     }
 }
