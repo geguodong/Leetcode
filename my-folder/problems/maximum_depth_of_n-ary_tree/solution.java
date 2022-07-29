f9d731928223015f861dcd2bba0d8bdc8d9f91df
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-29 18:56:48 +0000

    Sync LeetCode submission - Maximum Depth of N-ary Tree (java)

--- my-folder/problems/maximum_depth_of_n-ary_tree/solution.java
+++ my-folder/problems/maximum_depth_of_n-ary_tree/solution.java
@@ -17,32 +17,24 @@ class Node {
 };
 */
 
-
-
-/*
-Iteration
-Time complexity : O(N).
-Space complexity : O(N).
-*/
 class Solution {
-  public int maxDepth(Node root) {
-    Queue<Pair<Node, Integer>> stack = new LinkedList<>();
-    if (root != null) {
-      stack.add(new Pair(root, 1));
-    }
-
-    int depth = 0;
-    while (!stack.isEmpty()) {
-      Pair<Node, Integer> current = stack.poll();
-      root = current.getKey();
-      int current_depth = current.getValue();
-      if (root != null) {
-        depth = Math.max(depth, current_depth);
-        for (Node c : root.children) {
-          stack.add(new Pair(c, current_depth + 1));    
+    public int maxDepth(Node root) {
+        Queue<Pair<Node, Integer>> stack = new LinkedList<>();
+        if (root != null) {
+            stack.add(new Pair(root, 1));
         }
-      }
+        int depth = 0;
+        while (!stack.isEmpty()) {
+            Pair<Node, Integer> current = stack.poll();
+            root = current.getKey();
+            int current_depth = current.getValue();
+            if (root != null) {
+                depth = Math.max(depth, current_depth);
+                for (Node c : root.children) {
+                    stack.add(new Pair(c, current_depth + 1));
+                }
+            }
+        }
+        return depth;
     }
-    return depth;
-  }
 }
