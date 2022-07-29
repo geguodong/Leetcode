cb289ab5fe560a949350c68d750c7f4e634330c6
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-29 22:45:21 +0000

    Sync LeetCode submission - Minimum Add to Make Parentheses Valid (java)

--- my-folder/problems/minimum_add_to_make_parentheses_valid/solution.java
+++ my-folder/problems/minimum_add_to_make_parentheses_valid/solution.java
@@ -1,20 +1,14 @@
-/*
-Time Complexity: O(N), where NN is the length of S.
-Space Complexity: O(1).
-*/
-
 class Solution {
     public int minAddToMakeValid(String S) {
         int ans = 0, bal = 0;
         for (int i = 0; i < S.length(); ++i) {
-            bal += S.charAt(i) == '(' ? 1 : -1;
+             bal += S.charAt(i) == '(' ? 1 : -1;
             // It is guaranteed bal >= -1
             if (bal == -1) {
                 ans++;
                 bal++;
             }
         }
-
         return ans + bal;
     }
 }
