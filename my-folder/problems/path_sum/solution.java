2e656dfe4f778935fa81dd1d5f7c5e74f6642bfb
geguodong <37557614+geguodong@users.noreply.github.com>
2022-07-29 17:32:09 +0000

    Sync LeetCode submission - Path Sum (java)

--- my-folder/problems/path_sum/solution.java
+++ my-folder/problems/path_sum/solution.java
@@ -14,43 +14,17 @@
  * }
  */
 
-/*
-Iterations
- 
- Time complexity : the same as the recursion approach \mathcal{O}(N)O(N).
-Space complexity : \mathcal{O}(N)O(N) since in the worst case, when the tree is completely unbalanced, e.g. each node has only one child node, we would keep all NN nodes in the stack. But in the best case (the tree is balanced), the height of the tree would be \log(N)log(N). Therefore, the space complexity in this case would be \mathcal{O}(\log(N))O(log(N)).
-
-*/
-// Iterator
-// O(N)
-// Worse O(N)  Average O(logn)
 class Solution {
-  public boolean hasPathSum(TreeNode root, int sum) {
-    if (root == null)
-      return false;
-
-    LinkedList<TreeNode> node_stack = new LinkedList();
-    LinkedList<Integer> sum_stack = new LinkedList();
-    node_stack.add(root);
-    sum_stack.add(sum - root.val);
-
-    TreeNode node;
-    int curr_sum;
-    while ( !node_stack.isEmpty() ) {
-      node = node_stack.pollLast();
-      curr_sum = sum_stack.pollLast();
-      if ((node.right == null) && (node.left == null) && (curr_sum == 0))
-        return true;
-
-      if (node.right != null) {
-        node_stack.add(node.right);
-        sum_stack.add(curr_sum - node.right.val);
-      }
-      if (node.left != null) {
-        node_stack.add(node.left);
-        sum_stack.add(curr_sum - node.left.val);
-      }
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        
+        sum -= root.val;
+        if ((root.left == null) && (root.right == null)) {
+            return (sum == 0);
+        }
+        
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
-    return false;
-  }
 }
