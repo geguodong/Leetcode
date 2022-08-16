c06f3810c033340733172201d9243bbe4f40ab3d
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-16 21:52:36 +0000

    Sync LeetCode submission - Symmetric Tree (java)

--- my-folder/problems/symmetric_tree/solution.java
+++ my-folder/problems/symmetric_tree/solution.java
@@ -15,32 +15,24 @@
  */
 
 /*
-Time complexity : O(n)Because we traverse the entire input tree once, the total run time is O(n), where nn is the total number of nodes in the tree.
-
-Space complexity : The number of recursive calls is bound by the height of the tree. In the worst case, the tree is linear and the height is in O(n) Therefore, space complexity due to recursive calls on the stack is O(n) in the worst case.
+Time : O(N) all nodes need to traverse once
+Space : O(n) stack for height, worst case would be O(n)
 */
-
-/*
-Time complexity : O(n)O. Because we traverse the entire input tree once, the total run time is O(n), where nn is the total number of nodes in the tree.
-
-Space complexity : There is additional space required for the search queue. In the worst case, we have to insert O(n) nodes in the queue. Therefore, space complexity is O(n).
-*/
-class Solution {  
+class Solution {
     public boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode t1 = q.poll();
-            TreeNode t2 = q.poll();
-            if (t1 == null && t2 == null) continue;
-            if (t1 == null || t2 == null) return false;
-            if (t1.val != t2.val) return false;
-            q.add(t1.left);
-            q.add(t2.right);
-            q.add(t1.right);
-            q.add(t2.left);
+        if(root == null) {
+            return true;
         }
-    return true;
-    }    
+        return helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && helper(p.left, q.right) && helper(p.right, q.left);
+    }
 }
