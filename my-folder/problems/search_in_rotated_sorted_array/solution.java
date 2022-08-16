9be09e4422cba9d2b1def6e7f928b5c4298af6ce
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-16 23:10:44 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (java)

--- my-folder/problems/search_in_rotated_sorted_array/solution.java
+++ my-folder/problems/search_in_rotated_sorted_array/solution.java
@@ -1,21 +1,33 @@
 // Binary Search
 // O(logn) O(1)
-
 class Solution {
-  public int search(int[] nums, int target) {
-    int start = 0, end = nums.length - 1;
-    while (start <= end) {
-      int mid = start + (end - start) / 2;
-      if (nums[mid] == target) return mid;
-      else if (nums[mid] >= nums[start]) {
-        if (target >= nums[start] && target < nums[mid]) end = mid - 1;
-        else start = mid + 1;
-      }
-      else {
-        if (target <= nums[end] && target > nums[mid]) start = mid + 1;
-        else end = mid - 1;
-      }
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            
+            if(nums[mid] >= nums[left]) {
+                if(target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if(target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            
+        }
+        return -1;
     }
-    return -1;
-  }
 }
+
+
