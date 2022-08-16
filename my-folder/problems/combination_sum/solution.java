1e87821572455310df6d71a71c7cd4ea0e0ac22d
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-16 21:25:47 +0000

    Sync LeetCode submission - Combination Sum (java)

--- my-folder/problems/combination_sum/solution.java
+++ my-folder/problems/combination_sum/solution.java
@@ -1,32 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
         List<List<Integer>> res = new ArrayList<>();
         LinkedList<Integer> path = new LinkedList<>();
-        if(candidates.length == 0) {
-            return res;
-        }
-        backtrack(candidates, 0,  res, path, 0, target);
+        backtrack(candidates, 0, target, path, res);
         return res;
     }
     
-    private void backtrack(int[] candidates, int start, List<List<Integer>> res, LinkedList<Integer> path, int curSum, int target) {
-        if(curSum == target) {
+    private void backtrack(int[] candidates, int start, int remain, LinkedList<Integer> path, List<List<Integer>> res) {
+        if(remain == 0) {
             res.add(new LinkedList<>(path));
-            return;
-        }
-        
-        if(curSum > target) {
-            return;
         }
         
         for(int i = start; i < candidates.length; i++) {
-            curSum += candidates[i];
+            if(candidates[i] > remain) {
+                continue;
+            }
+            
             path.add(candidates[i]);
-            backtrack(candidates, i, res, path, curSum, target);
-            curSum -= candidates[i];
+            backtrack(candidates, i, remain - candidates[i], path, res);
             path.removeLast();
         }
-    } 
-    
+    }
 }
