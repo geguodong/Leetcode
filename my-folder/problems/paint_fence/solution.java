651b3979a99e6efb7d13e6bea7d33a903a7ae8a6
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-17 22:28:15 +0000

    Sync LeetCode submission - Paint Fence (java)

---
+++ my-folder/problems/paint_fence/solution.java
@@ -0,0 +1,18 @@
+// bottom - up dp with  Memoization
+// O(n) o(1)
+class Solution {
+    public int numWays(int n, int k) {
+        if (n == 1) return k;
+        
+        int twoPostsBack = k;
+        int onePostBack = k * k;
+        
+        for(int i = 3; i <= n; i++) {
+            int curr = (k - 1) * (onePostBack + twoPostsBack);
+            twoPostsBack = onePostBack;
+            onePostBack = curr;
+        }
+        
+        return onePostBack;
+    }
+}
