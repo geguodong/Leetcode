278c8184c1d6960adf1cc5d1a1e43ff34c803a03
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-17 00:15:08 +0000

    Sync LeetCode submission - Find Right Interval (java)

---
+++ my-folder/problems/find_right_interval/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+  public int[] findRightInterval(int[][] intervals) {
+    TreeMap<Integer, Integer> starts = new TreeMap<>();
+    int res[] = new int[intervals.length];
+    for (int i = 0; i < intervals.length; i++) {
+      starts.put(intervals[i][0], i);
+    }
+    for (int i = 0; i < intervals.length; i++) {
+      Map.Entry<Integer, Integer> pos = starts.ceilingEntry(intervals[i][1]);
+      res[i] = pos == null ? -1 : pos.getValue();
+    }
+    return res;
+  }
+}
