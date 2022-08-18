7799a472d1e62e2848f4be66b5493bc31200f961
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-18 04:32:46 +0000

    Sync LeetCode submission - Final Prices With a Special Discount in a Shop (java)

--- my-folder/problems/final_prices_with_a_special_discount_in_a_shop/solution.java
+++ my-folder/problems/final_prices_with_a_special_discount_in_a_shop/solution.java
@@ -1,14 +1,26 @@
 class Solution {
+    
     public int[] finalPrices(int[] prices) {
-		for (int i = 0; i < prices.length; i++) {
-			for (int j = i+1; j < prices.length; j++) {
-				if (prices[j] <= prices[i]) {					
-                    prices[i] -= prices[j];
-                    break;
-				}
-			}			
-		}
-
-		return prices;
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i=prices.length-1; i>=0; i--) {
+            
+            while (!stack.isEmpty() && stack.peek() > prices[i]) {
+                
+                stack.pop();
+            }
+            
+            int price = prices[i];
+            
+            if (!stack.isEmpty()) {
+                
+                prices[i] = prices[i] - stack.peek();  
+            }
+            
+            stack.push(price);
+        }
+        
+        return prices;
     }
 }
