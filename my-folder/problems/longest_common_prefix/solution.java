d97c0903e5b17818fc9bd7e703384a903983a814
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-20 00:13:54 +0000

    Sync LeetCode submission - Longest Common Prefix (java)

--- my-folder/problems/longest_common_prefix/solution.java
+++ my-folder/problems/longest_common_prefix/solution.java
@@ -1,6 +1,6 @@
 /*
-Time complexity : O(S)O(S) , where S is the sum of all characters in all strings. In the worst case there will be nn equal strings with length mm and the algorithm performs S = m \cdot nS=m⋅n character comparisons. Even though the worst case is still the same as Approach 1, in the best case there are at most n \cdot minLenn⋅minLen comparisons where minLenminLen is the length of the shortest string in the array.
-Space complexity : O(1)O(1). We only used constant extra space.
+Time complexity : O(S) , where S is the sum of all characters in all strings. In the worst case there will be nn equal strings with length mm and the algorithm performs S=m⋅n character comparisons. Even though the worst case is still the same as Approach 1, in the best case there are at most n \cdot minLenn⋅minLen comparisons where minLenminLen is the length of the shortest string in the array.
+Space complexity : O(1). We only used constant extra space.
 */
 
 class Solution {
@@ -9,8 +9,9 @@ class Solution {
         for (int i = 0; i < strs[0].length() ; i++){
             char c = strs[0].charAt(i);
             for (int j = 1; j < strs.length; j ++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != c)
-                    return strs[0].substring(0, i);             
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }                
             }
         }
         return strs[0];
