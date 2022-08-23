cc3d1820aa1b2bf02149c5c6af2fd4a7c22785ec
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-23 22:07:24 +0000

    Sync LeetCode submission - Top K Frequent Elements (java)

--- my-folder/problems/top_k_frequent_elements/solution.java
+++ my-folder/problems/top_k_frequent_elements/solution.java
@@ -1,36 +1,26 @@
-// Heap
-// O(Nlogk) O(n + k)
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // O(1) time
-        if (k == nums.length) {
+        if(nums.length <= k) {
             return nums;
         }
         
-        // 1. build hash map : character and how often it appears
-        // O(N) time
-        Map<Integer, Integer> count = new HashMap();
-        for (int n: nums) {
-          count.put(n, count.getOrDefault(n, 0) + 1);
+        Map<Integer, Integer> count = new HashMap<>();
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> count.get(a) - count.get(b));
+        for(int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
-
-        // init heap 'the less frequent element first'
-        Queue<Integer> heap = new PriorityQueue<>(
-            (n1, n2) -> count.get(n1) - count.get(n2));
-
-        // 2. keep k top frequent elements in the heap
-        // O(N log k) < O(N log N) time
-        for (int n: count.keySet()) {
-          heap.add(n);
-          if (heap.size() > k) heap.poll();    
+        
+        for(int n : count.keySet()) {
+            heap.add(n);
+            if(heap.size() > k) {
+                heap.poll();
+            }
         }
-
-        // 3. build an output array
-        // O(k log k) time
-        int[] top = new int[k];
-        for(int i = k - 1; i >= 0; --i) {
-            top[i] = heap.poll();
+        
+        int[] topK = new int[heap.size()];
+        for(int i = heap.size() - 1; i >= 0; i--) {
+            topK[i] = heap.poll();
         }
-        return top;
+        return topK;
     }
 }
