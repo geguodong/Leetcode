0b98d3487202f9be9c242a0092c1b473bafab819
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-23 04:58:37 +0000

    Sync LeetCode submission - Range Sum Query 2D - Immutable (java)

--- my-folder/problems/range_sum_query_2d_-_immutable/solution.java
+++ my-folder/problems/range_sum_query_2d_-_immutable/solution.java
@@ -1,29 +1,22 @@
-
-
+// O(1)   :   O(MN) pre-processing
+// O(MN)
 class NumMatrix {
-    // 定义：preSum[i][j] 记录 matrix 中子矩阵 [0, 0, i-1, j-1] 的元素和
-    private int[][] preSum;
-    
+    private int[][] dp;
+
     public NumMatrix(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        if (m == 0 || n == 0) return;
-        // 构造前缀和矩阵
-        preSum = new int[m + 1][n + 1];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                // 计算每个矩阵 [0, 0, i, j] 的元素和
-                preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] + matrix[i - 1][j - 1] - preSum[i-1][j-1];
+        if (matrix.length == 0 || matrix[0].length == 0) return;
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                dp[r + 1][c + 1] = dp[r + 1][c] + dp[r][c + 1] + matrix[r][c] - dp[r][c];
             }
         }
     }
-    
-    // 计算子矩阵 [x1, y1, x2, y2] 的元素和
-    public int sumRegion(int x1, int y1, int x2, int y2) {
-        // 目标矩阵之和由四个相邻矩阵运算获得
-        return preSum[x2+1][y2+1] - preSum[x1][y2+1] - preSum[x2+1][y1] + preSum[x1][y1];
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
     }
 }
-
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
