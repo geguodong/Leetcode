4f93e59d3be3be7628e9f68b66302c868730f790
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-24 04:30:16 +0000

    Sync LeetCode submission - Number of Islands (java)

--- my-folder/problems/number_of_islands/solution.java
+++ my-folder/problems/number_of_islands/solution.java
@@ -1,35 +1,34 @@
-
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
-            return 0;
+        int res = 0;
+        if(grid == null || grid.length == 0 || grid[0].length == 0) {
+            return res;
         }
-        int count = 0;
-        int[][] directions = new int[][]{{0,1}, {1,0}, {-1,0}, {0,-1}};
+        
         int rows = grid.length;
         int cols = grid[0].length;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    grid[i][j] = '0';
+        int[][] directions = new int[][]{{-1,0}, {1,0}, {0,1}, {0,-1}};
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(grid[i][j] == '1') {
+                    res++;
                     Queue<Integer> queue = new LinkedList<>();
                     queue.add(i * cols + j);
-                    
-                    while (queue.size() != 0) {
-                        int cur = queue.remove();
-                        for (int[] dir : directions) {
-                            int r = cur / cols + dir[0];
-                            int c = cur % cols + dir[1];
-                            if (r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == '1') {
+                    while(!queue.isEmpty()) {
+                        int cur = queue.poll();
+                        grid[cur/cols][cur%cols] = '0';
+                        for(int[] dir : directions) {
+                            int r = cur/cols + dir[0];
+                            int c = cur%cols + dir[1];
+                            if(r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == '1') {
+                                queue.add(r*cols + c);
                                 grid[r][c] = '0';
-                                queue.add(r * cols + c);
                             }
                         }
                     }
                 }
             }
         }
-        return count;
+        return res;
     }
 }
