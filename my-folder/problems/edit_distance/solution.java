06e81ff4856a973381544351ae6db31920564433
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-24 04:48:49 +0000

    Sync LeetCode submission - Edit Distance (java)

--- my-folder/problems/edit_distance/solution.java
+++ my-folder/problems/edit_distance/solution.java
@@ -1,32 +1,37 @@
+// DP
+// O(MN) O(MN)
 class Solution {
-    int minDistance(String s1, String s2) {
-        int m = s1.length(), n = s2.length();
-        // 定义：s1[0..i] 和 s2[0..j] 的最小编辑距离是 dp[i+1][j+1]
-        int[][] dp = new int[m + 1][n + 1];
-        // base case 
-        for (int i = 1; i <= m; i++)
-            dp[i][0] = i;
-        for (int j = 1; j <= n; j++)
-            dp[0][j] = j;
-        // 自底向上求解
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s1.charAt(i-1) == s2.charAt(j-1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = min(
-                        dp[i - 1][j] + 1,
-                        dp[i][j - 1] + 1,
-                        dp[i - 1][j - 1] + 1
-                    );
-                }
-            }
-        }
-        // 储存着整个 s1 和 s2 的最小编辑距离
-        return dp[m][n];
+  public int minDistance(String word1, String word2) {
+    int n = word1.length();
+    int m = word2.length();
+
+    // if one of the strings is empty
+    if (n * m == 0)
+      return n + m;
+
+    // array to store the convertion history
+    int [][] d = new int[n + 1][m + 1];
+
+    // init boundaries
+    for (int i = 0; i < n + 1; i++) {
+      d[i][0] = i;
+    }
+    for (int j = 0; j < m + 1; j++) {
+      d[0][j] = j;
     }
 
-    int min(int a, int b, int c) {
-        return Math.min(a, Math.min(b, c));
+    // DP compute 
+    for (int i = 1; i < n + 1; i++) {
+      for (int j = 1; j < m + 1; j++) {
+        int left = d[i - 1][j] + 1;
+        int down = d[i][j - 1] + 1;
+        int left_down = d[i - 1][j - 1];
+        if (word1.charAt(i - 1) != word2.charAt(j - 1))
+          left_down += 1;
+        d[i][j] = Math.min(left, Math.min(down, left_down));
+
+      }
     }
+    return d[n][m];
+  }
 }
