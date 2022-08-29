1ff4627736f95c3b66acee7cdfb4661728d9f1ff
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-29 23:46:54 +0000

    Sync LeetCode submission - Swim in Rising Water (java)

---
+++ my-folder/problems/swim_in_rising_water/solution.java
@@ -0,0 +1,38 @@
+/*
+Heap
+O(n^2 logN)
+O(N^2)
+
+*/
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int N = grid.length;
+        Set<Integer> seen = new HashSet();
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((k1, k2) ->
+                grid[k1 / N][k1 % N] - grid[k2 / N][k2 % N]);
+        pq.offer(0);
+        int ans = 0;
+        int[][] directions = new int[][]{{-1,0}, {1,0}, {0,1}, {0,-1}};
+        int[] dr = new int[]{1, -1, 0, 0};
+        int[] dc = new int[]{0, 0, 1, -1};
+
+        while (!pq.isEmpty()) {
+            int k = pq.poll();
+            int r = k / N, c = k % N;
+            ans = Math.max(ans, grid[r][c]);
+            if (r == N-1 && c == N-1) return ans;
+            
+            for(int[] dir : directions) {
+                int cr = r + dir[0];
+                int cc = c + dir[1];
+                int ck = cr * N + cc;
+                if (0 <= cr && cr < N && 0 <= cc && cc < N && !seen.contains(ck)) {
+                    pq.offer(ck);
+                    seen.add(ck);
+                }
+            }
+        }
+
+        return -1;
+    }
+}
