547fbd696fd680d8d560f0c42da8777caed5a197
geguodong <37557614+geguodong@users.noreply.github.com>
2022-08-29 22:32:37 +0000

    Sync LeetCode submission - Next Greater Element III (java)

---
+++ my-folder/problems/next_greater_element_iii/solution.java
@@ -0,0 +1,48 @@
+/*
+From end, find the first index which is less than next digit;
+then from end, find the first one just greater than the digit;
+swap them, and then reverse from i + 1
+check if throw exception, return -1;
+        try {
+            return Integer.parseInt(new String(a));
+        } catch (Exception e) {
+            return -1;
+        }
+
+O(n) O(n)       
+*/
+public class Solution {
+    public int nextGreaterElement(int n) {
+        char[] a = ("" + n).toCharArray();
+        int i = a.length - 2;
+        while (i >= 0 && a[i + 1] <= a[i]) {
+            i--;
+        }
+        if (i < 0)
+            return -1;
+        int j = a.length - 1;
+        while (j >= 0 && a[j] <= a[i]) {
+            j--;
+        }
+        swap(a, i, j);
+        reverse(a, i + 1);
+        try {
+            return Integer.parseInt(new String(a));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+    private void reverse(char[] a, int start) {
+        int i = start, j = a.length - 1;
+        while (i < j) {
+            swap(a, i, j);
+            i++;
+            j--;
+        }
+    }
+    private void swap(char[] a, int i, int j) {
+        char temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
