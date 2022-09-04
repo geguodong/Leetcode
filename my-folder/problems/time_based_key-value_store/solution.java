f6b43b845ad0a4a1aa4f35cb0beae07fe7968ccf
geguodong <37557614+geguodong@users.noreply.github.com>
2022-09-04 17:14:17 +0000

    Sync LeetCode submission - Time Based Key-Value Store (java)

--- my-folder/problems/time_based_key-value_store/solution.java
+++ my-folder/problems/time_based_key-value_store/solution.java
@@ -20,48 +20,30 @@
  * String param_2 = obj.get(key,timestamp);
  */
 
-class TimeMap {
 
-    static class Value {
-        String val;
-        int timestamp;
-        Value(String val, int timestamp) {
-            this.val = val;
-            this.timestamp = timestamp;
-        }
-    }
-    
-    Map<String, List<Value>> map;
-    
+class TimeMap {
+    HashMap<String, TreeMap<Integer, String>> timer;
     public TimeMap() {
-        map = new HashMap<>();
+        timer = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        Value v = new Value(value, timestamp);
-        List<Value> list = map.getOrDefault(key, new ArrayList<>());
-        list.add(v);
-        map.put(key, list);
-     //   map.computeIfAbsent(key, k -> new ArrayList<>()).add(v);
+        TreeMap treemap = timer.getOrDefault(key, new TreeMap());
+        treemap.put(timestamp, value);
+        timer.put(key, treemap);
+      //  timer.putIfAbsent(key, new TreeMap<>());
+      //  timer.get(key).put(timestamp, value);
+       
     }
     
     public String get(String key, int timestamp) {
-        List<Value> values = map.get(key);
-        if (values == null || values.size() == 0) {
-            return "";
+        String get = "";
+        if(timer.containsKey(key)){
+            Integer floorKey;
+            floorKey = timer.get(key).floorKey(timestamp);
+            if(floorKey != null)
+                get = timer.get(key).get(floorKey);
         }
-        String res = "";
-        int lo = 0;
-        int hi = values.size() - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (values.get(mid).timestamp <= timestamp) {
-                res = values.get(mid).val;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-        return res;
+        return get;
     }
 }
