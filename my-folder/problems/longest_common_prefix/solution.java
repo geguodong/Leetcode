91973c5f5ce6d399380e40381e272fce7d6a3df5
geguodong <37557614+geguodong@users.noreply.github.com>
2022-09-05 21:16:35 +0000

    Sync LeetCode submission - Longest Common Prefix (java)

--- my-folder/problems/longest_common_prefix/solution.java
+++ my-folder/problems/longest_common_prefix/solution.java
@@ -1,19 +1,30 @@
-/*
-Time complexity : O(S) , where S is the sum of all characters in all strings. In the worst case there will be nn equal strings with length mm and the algorithm performs S=m⋅n character comparisons. Even though the worst case is still the same as Approach 1, in the best case there are at most n \cdot minLenn⋅minLen comparisons where minLenminLen is the length of the shortest string in the array.
-Space complexity : O(1). We only used constant extra space.
-*/
-
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        for (int i = 0; i < strs[0].length() ; i++){
-            char c = strs[0].charAt(i);
-            for (int j = 1; j < strs.length; j ++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != c) {
-                    return strs[0].substring(0, i);
-                }                
+        for(int i = 1; i < strs.length; i++) {
+            strs[i] = compare(strs[i - 1], strs[i]);
+        }
+        return strs[strs.length - 1];
+    }
+    
+    public String compare(String s1, String s2) {
+        if(s1.length() > s2.length()) {
+            return compare(s2, s1);
+        }
+        String res = "";
+        
+        int index = 0;
+        while(index < s1.length()) {
+            if(s1.charAt(index) != s2.charAt(index)) {
+                System.out.println("res : " + s1.charAt(index));
+                res = s1.substring(0, index);
+                return res;
+            } else {
+                index++;
             }
         }
-        return strs[0];
+        res = s1.substring(0, index);
+        System.out.println("res : " + res);
+        return res;
+        
     }
 }
