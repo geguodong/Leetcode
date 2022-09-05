6362abe63955a9d25489d511bb09b7a1fe93fd4c
geguodong <37557614+geguodong@users.noreply.github.com>
2022-09-05 21:29:43 +0000

    Sync LeetCode submission - Rotate Image (java)

--- my-folder/problems/rotate_image/solution.java
+++ my-folder/problems/rotate_image/solution.java
@@ -1,28 +1,26 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        transpose(matrix);
-        reflect(matrix);
+        folder(matrix);
+        folder2(matrix);
     }
     
-    public void transpose(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int tmp = matrix[j][i];
-                matrix[j][i] = matrix[i][j];
-                matrix[i][j] = tmp;
+    public void folder(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix.length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length - 1 - j];
+                matrix[i][matrix[0].length - 1 - j] = temp;
             }
         }
     }
     
-    public void reflect(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - j - 1];
-                matrix[i][n - j - 1] = tmp;
-            }
+    public void folder2(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix.length - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length - 1 - j][matrix.length - 1 - i];
+                matrix[matrix.length - 1 - j][matrix.length - 1 - i] = temp;
+            } 
         }
     }
 }
