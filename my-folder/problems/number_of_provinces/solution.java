055d3de618ddbcb13508597549fc7d920ebac42f
geguodong <37557614+geguodong@users.noreply.github.com>
2022-09-05 05:37:25 +0000

    Sync LeetCode submission - Number of Provinces (java)

---
+++ my-folder/problems/number_of_provinces/solution.java
@@ -0,0 +1,25 @@
+// O(n^2) 
+// O(N)
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int[] visited = new int[n];
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(visited[i] == 0) {
+                dfs(isConnected, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(int[][] isConnected, int[] visited, int i) {
+        for(int j = 0; j < isConnected.length; j++) {
+            if(isConnected[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(isConnected, visited, j);
+            }
+        }
+    }
+}
