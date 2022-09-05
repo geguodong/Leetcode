a6ad957ef825f45ab81790cbec9fea0783c0c0f6
geguodong <37557614+geguodong@users.noreply.github.com>
2022-09-05 19:37:44 +0000

    Sync LeetCode submission - Most Profit Assigning Work (java)

--- my-folder/problems/most_profit_assigning_work/solution.java
+++ my-folder/problems/most_profit_assigning_work/solution.java
@@ -3,23 +3,28 @@ Time Complexity: O(NlogN+QlogQ), where N is the number of jobs, and Q is the num
 Space Complexity: O(N), the additional space used by jobs.
 */
 
-import java.awt.Point;
-
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         int N = difficulty.length;
-        Point[] jobs = new Point[N];
+        List<List<Integer>> jobs = new ArrayList<>();
         for (int i = 0; i < N; ++i) {
-            jobs[i] = new Point(difficulty[i], profit[i]);
+            jobs.add(Arrays.asList(difficulty[i], profit[i]));
         }
             
-        Arrays.sort(jobs, (a, b) -> a.x - b.x);
+        Collections.sort(jobs, (a, b) -> {
+            if(a.get(0) < b.get(0)) {
+                return -1;
+            }
+            return 1;
+           // return a.get(0) - b.get(0);
+        });
         Arrays.sort(worker);
 
-        int ans = 0, i = 0, best = 0;
+        int ans = 0, i = 0, best = 0; // We don't reset them O(N + Q)
         for (int skill: worker) {
-            while (i < N && skill >= jobs[i].x) {
-                best = Math.max(best, jobs[i++].y);
+            while (i < N && skill >= jobs.get(i).get(0)) {
+                best = Math.max(best, jobs.get(i).get(1));
+                i++;
             }
             ans += best;
         }
