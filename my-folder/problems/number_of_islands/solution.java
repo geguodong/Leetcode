edb85369cc26cdda97fd7cd4509b81d272cb50a1
geguodong <37557614+geguodong@users.noreply.github.com>
2022-09-09 22:38:56 +0000

    Sync LeetCode submission - Number of Islands (java)

--- my-folder/problems/number_of_islands/solution.java
+++ my-folder/problems/number_of_islands/solution.java
@@ -7,28 +7,28 @@ class Solution {
         
         int rows = grid.length;
         int cols = grid[0].length;
-        int[][] directions = new int[][]{{-1,0}, {1,0}, {0,1}, {0,-1}};
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 if(grid[i][j] == '1') {
                     res++;
-                    Queue<Integer> queue = new LinkedList<>();
-                    queue.add(i * cols + j);
-                    while(!queue.isEmpty()) {
-                        int cur = queue.poll();
-                        grid[cur/cols][cur%cols] = '0';
-                        for(int[] dir : directions) {
-                            int r = cur/cols + dir[0];
-                            int c = cur%cols + dir[1];
-                            if(r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == '1') {
-                                queue.add(r*cols + c);
-                                grid[r][c] = '0';
-                            }
-                        }
-                    }
+                    dfs(grid, i, j);
                 }
             }
         }
         return res;
+     
+    }
+    
+    private void dfs(char[][] grid, int r, int c) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if(r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+        dfs(grid, r + 1, c);
     }
 }
