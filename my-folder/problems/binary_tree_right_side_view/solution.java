9911743a09d01c0b7c0c4c7a5a8b406b024a23f6
geguodong <37557614+geguodong@users.noreply.github.com>
2022-09-09 22:29:54 +0000

    Sync LeetCode submission - Binary Tree Right Side View (java)

--- my-folder/problems/binary_tree_right_side_view/solution.java
+++ my-folder/problems/binary_tree_right_side_view/solution.java
@@ -13,23 +13,24 @@
  *     }
  * }
  */
-// BFS level order traverse 
-// O(n) O(n)
+
+// BFS Level order traverse 
+// O(N) O(N)
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if(root == null) {
             return res;
         }
+        
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         
         while(!queue.isEmpty()) {
             int size = queue.size();
             while(size > 0) {
-                size--;
                 TreeNode cur = queue.poll();
-                if(size == 0) {
+                if(size == 1) {
                     res.add(cur.val);
                 }
                 if(cur.left != null) {
@@ -38,8 +39,8 @@ class Solution {
                 if(cur.right != null) {
                     queue.add(cur.right);
                 }
+                size--;
             }
-         
         }
         return res;
     }
