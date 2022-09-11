12a0240a85a4b2032afbd85af9d9a1a67da9508b
geguodong <37557614+geguodong@users.noreply.github.com>
2022-09-11 18:52:40 +0000

    Sync LeetCode submission - Basic Calculator II (java)

--- my-folder/problems/basic_calculator_ii/solution.java
+++ my-folder/problems/basic_calculator_ii/solution.java
@@ -1,39 +1,31 @@
-// Use Stack
-// O(N) O(N)
+// O(N) O(1)
 class Solution {
     public int calculate(String s) {
-
-        if (s == null || s.isEmpty()) return 0;
-        int len = s.length();
-        Stack<Integer> stack = new Stack<Integer>();
-        int currentNumber = 0;
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        int length = s.length();
+        int currentNumber = 0, lastNumber = 0, result = 0;
         char operation = '+';
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < length; i++) {
             char currentChar = s.charAt(i);
             if (Character.isDigit(currentChar)) {
                 currentNumber = (currentNumber * 10) + (currentChar - '0');
             }
-            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == len - 1) {
-                if (operation == '-') {
-                    stack.push(-currentNumber);
-                }
-                else if (operation == '+') {
-                    stack.push(currentNumber);
-                }
-                else if (operation == '*') {
-                    stack.push(stack.pop() * currentNumber);
-                }
-                else if (operation == '/') {
-                    stack.push(stack.pop() / currentNumber);
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
                 }
                 operation = currentChar;
                 currentNumber = 0;
             }
         }
-        int result = 0;
-        while (!stack.isEmpty()) {
-            result += stack.pop();
-        }
+        result += lastNumber;
         return result;
     }
 }
