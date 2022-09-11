897cb7ff200af0b6138442fb40b2e0509019a497
geguodong <37557614+geguodong@users.noreply.github.com>
2022-09-11 07:48:45 +0000

    Sync LeetCode submission - Asteroid Collision (java)

--- my-folder/problems/asteroid_collision/solution.java
+++ my-folder/problems/asteroid_collision/solution.java
@@ -1,17 +1,18 @@
+// O(N)   O(N)
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-       Stack<Integer> st=new Stack();
+       Stack<Integer> st = new Stack();
         for(int val:asteroids){
             if(val>0){
               st.push(val);  
             } else {
-                while(st.size()>0 && st.peek()>0 && st.peek()<-val){
+                while(st.size() > 0 && st.peek() > 0 && st.peek() < -val){
                     st.pop();
                 }
-                if(st.size()>0 && st.peek()==-val){
+                if(st.size() > 0 && st.peek() == -val){
                     st.pop();
                 }
-                else if(st.size()>0 && st.peek()> -val){
+                else if(st.size() > 0 && st.peek() > -val){
                     continue;
                 } else {
                     st.push(val);
